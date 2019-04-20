4e5565056b707989974cde2a7c0ec3311089c205
Roshan Dathathri <roshan@cs.utexas.edu>
2019-04-19 19:30:40 -0500

    Cypher compiler: generating ANY node/edge

--- lonestar/experimental/cypher_compiler/CypherCompiler.h
+++ lonestar/experimental/cypher_compiler/CypherCompiler.h
@@ -36,8 +36,12 @@ class CypherCompiler {
             auto element_type = cypher_astnode_type(element);
             if (element_type == CYPHER_AST_NODE_PATTERN) {
                 auto label = cypher_ast_node_pattern_get_label(element, 0);
-                os << cypher_ast_label_get_name(label);
-                os << ",";
+                if (label != NULL) {
+                    os << cypher_ast_label_get_name(label);
+                    os << ",";
+                } else {
+                    os << "ANY,";
+                }
                 auto nameNode = cypher_ast_node_pattern_get_identifier(element);
                 if (nameNode != NULL) {
                     auto name = cypher_ast_identifier_get_name(nameNode);
@@ -47,8 +51,12 @@ class CypherCompiler {
                 }
             } else if (element_type == CYPHER_AST_REL_PATTERN) {
                 auto reltype = cypher_ast_rel_pattern_get_reltype(element, 0);
-                os << cypher_ast_reltype_get_name(reltype);
-                os << ",";
+                if (reltype != NULL) {
+                    os << cypher_ast_reltype_get_name(reltype);
+                    os << ",";
+                } else {
+                    os << "ANY,";
+                }
                 auto nameNode = cypher_ast_rel_pattern_get_identifier(element);
                 if (nameNode != NULL) {
                     auto name = cypher_ast_identifier_get_name(nameNode);

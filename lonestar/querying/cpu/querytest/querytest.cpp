06edfccd0bd2f16654db8cdd1605decb679fa5dc
Loc Hoang <l_hoang@utexas.edu>
2020-06-17 09:50:55 -0500

    clang-format on querytest

--- lonestar/querying/cpu/querytest/querytest.cpp
+++ lonestar/querying/cpu/querytest/querytest.cpp
@@ -17,7 +17,7 @@
 
 static const char* name = "DBGraph Testing";
 static const char* desc = "Testing DBGraph";
-static const char* url = "";
+static const char* url  = "";
 
 ////////////////////////////////////////////////////////////////////////////////
 // Command line args
@@ -28,35 +28,38 @@ namespace cll = llvm::cl;
 static cll::opt<std::string>
     filename(cll::Positional, cll::desc("<input graph>"), cll::Required);
 
-static cll::opt<std::string>
-    query("query", cll::desc("Cypher query"), cll::init(""));
+static cll::opt<std::string> query("query", cll::desc("Cypher query"),
+                                   cll::init(""));
 
 // TODO get this to work; requires library end to accept argument to where for
 // output
-//static cll::opt<std::string>
+// static cll::opt<std::string>
 //    edgefile(cll::Positional, cll::desc("Cypher query"), cll::Required);
 //
 
-static cll::opt<bool> isAttributedGraph("isAttributedGraph",
-                      cll::desc("Specifies that the passed in file is an attributed graph on disk "
-                                "(default false)"),
-                      cll::init(false));
+static cll::opt<bool> isAttributedGraph(
+    "isAttributedGraph",
+    cll::desc(
+        "Specifies that the passed in file is an attributed graph on disk "
+        "(default false)"),
+    cll::init(false));
 
-static cll::opt<std::string> queryFile("queryFile",
-                               cll::desc("File containing Cypher query to run"
-                                         "; takes precedence over query string"),
-                               cll::init(""));
+static cll::opt<std::string>
+    queryFile("queryFile",
+              cll::desc("File containing Cypher query to run"
+                        "; takes precedence over query string"),
+              cll::init(""));
 
-static cll::opt<bool> skipGraphSimulation("skipGraphSimulation",
-                      cll::desc("Do not use graph simulation "
-                                "(default false)"),
-                      cll::init(false));
-
+static cll::opt<bool>
+    skipGraphSimulation("skipGraphSimulation",
+                        cll::desc("Do not use graph simulation "
+                                  "(default false)"),
+                        cll::init(false));
 
 static cll::opt<uint32_t> numPages("numPages",
-                      cll::desc("Number of pages to pre-alloc "
-                                "(default 2500)"),
-                      cll::init(2500));
+                                   cll::desc("Number of pages to pre-alloc "
+                                             "(default 2500)"),
+                                   cll::init(2500));
 
 ////////////////////////////////////////////////////////////////////////////////
 // Main
@@ -83,7 +86,7 @@ int main(int argc, char** argv) {
   // 3 node labels: n1, n2, n3
   // 3 edge labels: e1, e2, e3
   // timestamps on edges are increasing order
-  if (queryFile != "" ) {
+  if (queryFile != "") {
     // read file into a std::string
     // https://stackoverflow.com/questions/2602013/read-whole-ascii-file-into-c-stdstring
     // TODO check if opened successfully (good C practice to do so)
@@ -92,9 +95,12 @@ int main(int argc, char** argv) {
     // putting into string stream lets you pull a string out of it
     querySS << queryStream.rdbuf();
 
-    galois::gInfo("Num matched subgraphs ", testGraph.runCypherQuery(querySS.str(), !skipGraphSimulation));
+    galois::gInfo(
+        "Num matched subgraphs ",
+        testGraph.runCypherQuery(querySS.str(), !skipGraphSimulation));
   } else if (query != "") {
-    galois::gInfo("Num matched subgraphs ", testGraph.runCypherQuery(query, !skipGraphSimulation));
+    galois::gInfo("Num matched subgraphs ",
+                  testGraph.runCypherQuery(query, !skipGraphSimulation));
   } else {
     galois::gInfo("No query specified");
   }

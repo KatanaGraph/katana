83bf6d5a5a08d0cee76296e3402547dd3b8bc2e4
Loc Hoang <l_hoang@utexas.edu>
2020-06-18 15:28:16 -0500

    querytest output, timers

--- lonestar/querying/cpu/querytest/querytest.cpp
+++ lonestar/querying/cpu/querytest/querytest.cpp
@@ -51,6 +51,12 @@ static cll::opt<std::string>
               cll::init(""));
 
 static cll::opt<bool>
+    output("output",
+           cll::desc("If true, write query count to file; only works if using "
+                     "listOfQueries argument (default false)"),
+           cll::init(false));
+
+static cll::opt<bool>
     skipGraphSimulation("skipGraphSimulation",
                         cll::desc("Do not use graph simulation "
                                   "(default false)"),
@@ -67,7 +73,8 @@ static cll::opt<uint32_t> numPages("numPages",
 
 //! given file with query, run query and return number of matches
 size_t processQueryFile(galois::graphs::DBGraph& testGraph,
-                        std::string fileToProcess) {
+                        std::string fileToProcess,
+                        std::string queryName = "Query") {
   galois::gInfo("Reading query file ", fileToProcess);
   // read file into a std::string
   // https://stackoverflow.com/questions/2602013/read-whole-ascii-file-into-c-stdstring
@@ -79,8 +86,12 @@ size_t processQueryFile(galois::graphs::DBGraph& testGraph,
   // putting into string stream lets you pull a string out of it
   querySS << queryStream.rdbuf();
 
+  galois::StatTimer timer((queryName + "_Timer").c_str());
+  timer.start();
   size_t numMatch =
       testGraph.runCypherQuery(querySS.str(), !skipGraphSimulation);
+  timer.stop();
+
   galois::gInfo("Num matched subgraphs ", numMatch);
   return numMatch;
 }
@@ -88,6 +99,10 @@ size_t processQueryFile(galois::graphs::DBGraph& testGraph,
 int main(int argc, char** argv) {
   galois::SharedMemSys G;
   LonestarStart(argc, argv, name, desc, url, &filename);
+
+  galois::StatTimer totalTime("TimerTotal");
+  totalTime.start();
+
 
   galois::graphs::DBGraph testGraph;
   if (!isAttributedGraph) {
@@ -115,9 +130,19 @@ int main(int argc, char** argv) {
       GALOIS_DIE("failed to open query list file ", listOfQueries);
     }
 
+    std::ofstream outputFile("queries.count");
+
     std::string curQueryFile;
     while (std::getline(queryFiles, curQueryFile)) {
-      processQueryFile(testGraph, curQueryFile);
+      // TODO drop file extension as well
+      std::string queryName =
+          curQueryFile.substr(curQueryFile.find_last_of("/\\") + 1);
+      size_t matched = processQueryFile(testGraph, curQueryFile, queryName);
+
+      // save query name and count for correctness checking
+      if (output) {
+        outputFile << queryName << " " << matched << "\n";
+      }
     }
   } else if (queryFile != "") {
     processQueryFile(testGraph, queryFile);
@@ -130,5 +155,11 @@ int main(int argc, char** argv) {
 
   galois::reportPageAlloc("MeminfoPost");
 
+  // TODO make this work regardless of how you pass in your queries
+  if (listOfQueries != "" && output) {
+    galois::gInfo("Query counts saved to queries.count");
+  }
+
+  totalTime.stop();
   return 0;
 }

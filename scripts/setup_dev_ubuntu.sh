f13c50bea4c4433b805cb3d1706acf4ba0c24fe3
Tyler Hunt <thunt@katanagraph.com>
2020-06-22 16:33:33 +0000

    scripts: actually install arrow and parquet
    Signed-off-by: Tyler Hunt <thunt@katanagraph.com>

--- scripts/setup_dev_ubuntu.sh
+++ scripts/setup_dev_ubuntu.sh
@@ -30,17 +30,20 @@ sudo apt upgrade cmake
 # alternatively:
 #   pip install cmake
 
-# installing arrow and parquet
+# installing arrow and parquet keyring
 curl -fL --output /tmp/arrow-keyring.deb \
   https://apache.bintray.com/arrow/ubuntu/apache-arrow-archive-keyring-latest-$(lsb_release --codename --short).deb \
   && sudo apt install -yq /tmp/arrow-keyring.deb \
   && rm /tmp/arrow-keyring.deb
+# installing arrow and parquet
+sudo apt update
+sudo apt install -yq libarrow-dev libparquet-dev
 
 # installing up-to-date llvm
 sudo apt-add-repository 'deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-10 main'
 curl -fL https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt update
-apt install -yq clang-10 clang++-10 clang-format-10 clang-tidy-10 llvm-10-dev
+sudo apt install -yq clang-10 clang++-10 clang-format-10 clang-tidy-10 llvm-10-dev
 
 # make clang-{tidy,format}-10 the default
 sudo update-alternatives --verbose --install /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-10 90

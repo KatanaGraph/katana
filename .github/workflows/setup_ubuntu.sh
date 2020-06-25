159caa36eec8d216cb028df08ca3e3168d47237f
Donald Nguyen <ddn0@users.noreply.github.com>
2020-06-25 09:26:13 -0700

    ci: Add system gcc to build matrix
    Along the way, be consistent about Release builds being tested always
    and Debug builds being tested more opportunistically.

--- .github/workflows/setup_ubuntu.sh
+++ .github/workflows/setup_ubuntu.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 apt install -yq ccache curl gcc-9 g++-9 libopenmpi-dev
-update-alternatives --verbose --install /usr/bin/gcc gcc /usr/bin/gcc-9 90
-update-alternatives --verbose --install /usr/bin/g++ g++ /usr/bin/g++-9 90
 
 curl -fL --output /tmp/arrow-keyring.deb https://apache.bintray.com/arrow/ubuntu/apache-arrow-archive-keyring-latest-$(lsb_release --codename --short).deb \
   && apt install -yq /tmp/arrow-keyring.deb \
@@ -17,9 +15,6 @@ apt update
 # conan packages do yet enable RTTI, which is required for boost
 # serialization.
 apt install -yq clang-10 clang++-10 clang-format-10 clang-tidy-10 llvm-10-dev
-
-update-alternatives --verbose --install /usr/bin/clang++ clang++ /usr/bin/clang++-10 90
-update-alternatives --verbose --install /usr/bin/clang clang /usr/bin/clang-10 90
 
 pip3 install --upgrade pip setuptools
 pip3 install conan==1.24

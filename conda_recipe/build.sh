f975f7e1fa5e08b539bb971c47def0000ed597d5
Arthur Peters <amp@singingwizard.org>
2020-07-07 17:06:38 -0500

    Enable ccache for conda builds.
    The builds should work fine in environments without ccache
    installed if needed and do not install ccache inside conda.
    They check for ccache and use it if it is available.
    
    [REWRITTEN COMMIT: incomplete subtree]

--- conda_recipe/build.sh
+++ conda_recipe/build.sh
@@ -1,10 +1,14 @@
 #! /bin/sh
-# export CCACHE_BASE_DIR=$SRC_DIR
-#  -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+CMAKE_CCACHE_OPTION=""
+if ccache -V > /dev/null 2> /dev/null; then
+    export CCACHE_BASEDIR="$(dirname "$SRC_DIR")"
+    CMAKE_CCACHE_OPTION="-DCMAKE_CXX_COMPILER_LAUNCHER=ccache"
+    echo "Enabling CCache."
+fi
 rm -rf build
 mkdir build
 cd build
 # Useful debugging addition to the below: -DCMAKE_VERBOSE_MAKEFILE=ON
-cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DUSE_ARCH=none -S $SRC_DIR
+cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DUSE_ARCH=none $CMAKE_CCACHE_OPTION -S "$SRC_DIR"
 make -j2
 make install
--- python/conda_recipe/build.sh
+++ python/conda_recipe/build.sh
@@ -1,5 +1,11 @@
 #! /bin/sh
+CMAKE_CCACHE_OPTION=""
+if ccache -V > /dev/null 2> /dev/null; then
+    export CCACHE_BASEDIR="$(dirname "$SRC_DIR")"
+    CMAKE_CCACHE_OPTION="-DCMAKE_CXX_COMPILER_LAUNCHER=ccache"
+    echo "Enabling CCache."
+fi
 cd $SRC_DIR
 rm -rf _skbuild
-export GALOIS_CMAKE_ARGS="-DBUILD_LIBGALOIS=OFF -DCMAKE_BUILD_TYPE=Release -DUSE_ARCH=none"
+export GALOIS_CMAKE_ARGS="-DBUILD_LIBGALOIS=OFF -DCMAKE_BUILD_TYPE=Release -DUSE_ARCH=none $CMAKE_CCACHE_OPTION"
 $PYTHON setup.py --verbose build_ext --verbose install -- -- -j2

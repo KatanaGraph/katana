ba96a2ad531750909e969f4cdfd3b26d2f7332cf
Arthur Peters <amp@singingwizard.org>
2020-07-17 17:33:10 -0500

    Add galois_cython magic for jupyter.
    This is needed since upcoming changes cause cython code to need
    "-I<numpy include dir>" which needs to be dynamically computed.
    
    [REWRITTEN COMMIT: incomplete subtree]

--- python/galois/__init__.py
+++ python/galois/__init__.py
@@ -1,1 +1,5 @@
-
+def load_ipython_extension(ipython):
+    import cython
+    cython.load_ipython_extension(ipython)
+    from .ipython import GaloisMagics
+    ipython.register_magics(GaloisMagics)
---
+++ python/galois/ipython.py
@@ -0,0 +1,17 @@
+from IPython.core.magic import Magics, magics_class, cell_magic
+
+import numpy
+
+@magics_class
+class GaloisMagics(Magics):
+    @cell_magic
+    def galois_cython(self, line, cell):
+        "Galois Cython code magic"
+        cython_magic = self.shell.find_cell_magic("cython")
+        # TODO: This needs to parse the options and add ours to the existing ones if needed.
+        header = """
+# distutils: extra_link_args=["-lgalois_shmem"]
+# distutils: extra_compile_args=["-I{}"]
+""".format(numpy.get_include())
+        cell = header + cell
+        return cython_magic(line, cell)

270e0f14bc5b06c399d4330abd66123e5e723bb3
Arthur Peters <amp@singingwizard.org>
2020-07-20 11:34:44 -0500

    Generate accurate and consistent GENERATED FILE headers.
    [REWRITTEN COMMIT: incomplete subtree]

--- python/generate-from-jinja.py
+++ python/generate-from-jinja.py
@@ -13,6 +13,12 @@ templateEnv = jinja2.Environment(loader=templateLoader)
 def all_combinations(l):
     return [x for n in range(len(l) + 1) for x in combinations(l, n)]
 
-templateEnv.globals.update(combinations=combinations, all_combinations=all_combinations)
+def generated_banner():
+    return "THIS FILE IS GENERATED FROM '{0}'. Make changes to that file instead of this one.".format(TEMPLATE_FILE)
+
+templateEnv.globals.update(
+    combinations=combinations,
+    all_combinations=all_combinations,
+    generated_banner=generated_banner)
 template = templateEnv.get_template(TEMPLATE_FILE)
 print(template.render())

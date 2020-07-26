2c72fb35e5163eee8b50efbf508e8df2b92c6d4c
Arthur Peters <amp@singingwizard.org>
2020-07-26 14:49:21 -0500

    Improve setActiveThreads handling in pytest.
    [REWRITTEN COMMIT: incomplete subtree]

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,3 +30,21 @@ def property_graph(request, small_inputs_dir):
     from galois.property_graph import PropertyGraph
     g = PropertyGraph(small_inputs_dir + "/propertygraphs/ldbc_003/meta")
     return g
+
+@pytest.fixture
+def threads_1():
+    from galois.shmem import setActiveThreads
+    setActiveThreads(1)
+    return True
+
+@pytest.fixture(autouse=True)
+def threads_default():
+    from galois.shmem import setActiveThreads
+    setActiveThreads(4)
+    return True
+
+@pytest.fixture
+def threads_many():
+    from galois.shmem import setActiveThreads
+    setActiveThreads(16)
+    return True

609534cece1d600c8d8df72331428093d6ef728d
Arthur Peters <amp@singingwizard.org>
2020-07-29 21:57:10 -0500

    Add data download utility to galois python library.
    [REWRITTEN COMMIT: incomplete subtree]

---
+++ python/galois/example_utils.py
@@ -0,0 +1,39 @@
+import os
+import tarfile
+import urllib.request
+
+__all__ = ["get_input"]
+
+
+def get_cache_directory():
+    # Use the CI paths if they exist, just to make CI integration easier.
+    ci_inputs_path = os.environ["HOME"] + "/.cache/graph"
+    if os.path.isdir(ci_inputs_path):
+        return ci_inputs_path
+    # Otherwise use a more specific path name
+    return os.environ["HOME"] + "/.cache/galois"
+
+
+def get_inputs_directory():
+    cache_dir = get_cache_directory()
+    inputs_dir = cache_dir + "/inputs"
+    if os.path.isdir(inputs_dir) and os.path.isfile(inputs_dir + "/propertygraphs/ldbc_003/meta"):
+        return inputs_dir
+    else:
+        fn, headers = urllib.request.urlretrieve("https://katana-ci-public.s3.us-east-1.amazonaws.com/inputs/katana-inputs-v2.1.tar.gz")
+        try:
+            with tarfile.open(fn) as tar:
+                tar.extractall(inputs_dir)
+        finally:
+            os.unlink(fn)
+    return inputs_dir
+
+
+def get_input(path):
+    """
+    Download the standard Galois inputs (with local caching on disk) and return a path to a file in that archive.
+
+    >>> from galois.property_graph import PropertyGraph
+    ... graph = PropertyGraph(get_input("propertygraphs/ldbc_003/meta"))
+    """
+    return get_inputs_directory() + "/" + path
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,35 +1,14 @@
 import pytest
 
-import os.path
-import os
-import tempfile
-import tarfile
-import urllib.request
+from galois.example_utils import get_input
 
-@pytest.fixture(scope="session")
-def small_inputs_dir(request):
-    small_inputs_dir = request.config.cache.get("small_inputs_dir", None)
-    if small_inputs_dir is not None and os.path.isdir(small_inputs_dir) and os.path.isfile(small_inputs_dir + "/propertygraphs/ldbc_003/meta"):
-        return small_inputs_dir
-    else:
-        # Check for CI cached graphs
-        ci_inputs_path = os.environ["HOME"] + "/.cache/graph/inputs"
-        if os.path.isdir(ci_inputs_path):
-            small_inputs_dir = ci_inputs_path
-        else:
-            # If there is no CI cache then download the data ourselves.
-            small_inputs_dir = tempfile.mkdtemp()
-            fn, headers = urllib.request.urlretrieve("https://katana-ci-public.s3.us-east-1.amazonaws.com/inputs/katana-inputs-v2.1.tar.gz")
-            with tarfile.open(fn) as tar:
-                tar.extractall(small_inputs_dir)
-        request.config.cache.set("small_inputs_dir", small_inputs_dir)
-    return small_inputs_dir
 
 @pytest.fixture
-def property_graph(request, small_inputs_dir):
+def property_graph():
     from galois.property_graph import PropertyGraph
-    g = PropertyGraph(small_inputs_dir + "/propertygraphs/ldbc_003/meta")
+    g = PropertyGraph(get_input("propertygraphs/ldbc_003/meta"))
     return g
+
 
 @pytest.fixture
 def threads_1():
@@ -37,11 +16,13 @@ def threads_1():
     setActiveThreads(1)
     return True
 
+
 @pytest.fixture(autouse=True)
 def threads_default():
     from galois.shmem import setActiveThreads
     setActiveThreads(4)
     return True
+
 
 @pytest.fixture
 def threads_many():

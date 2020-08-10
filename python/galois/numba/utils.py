102adc341f9da3548757317370c421d4dbe096e6
Arthur Peters <amp@singingwizard.org>
2020-08-10 14:27:44 -0500

    python: Add utility functions to dump llvm IR for numba functions.
    [REWRITTEN COMMIT: incomplete subtree]

--- python/galois/numba/utils.py
+++ python/galois/numba/utils.py
@@ -26,3 +26,21 @@ def interpret_numba_wrapper_tables(tables, globals=None):
 def interpret_numba_wrapper_table(Type, table):
     for name, func_type, impl_func_name, addr in table:
         Type.register_method(name, func_type, impl_func_name, addr=addr)
+
+
+def _dump_llvm(f, output_func):
+    d = f.inspect_llvm()
+    if isinstance(d, dict):
+        for ty, code in d.items():
+            output_func("\n===== {}\n{}".format(ty, code))
+    else:
+        output_func("\n" + d)
+
+
+def dump_numba_llvm(func):
+    out = print
+    module = __import__(func.__module__)
+    if hasattr(module, "_logger"):
+        out = module._logger.debug
+    if hasattr(func, "inspect_llvm"):
+        _dump_llvm(func, output_func=out)

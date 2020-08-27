79f10317e8c6b3848780c91a6a873645ba26599d
Loc Hoang <loc@katanagraph.com>
2020-08-27 16:25:56 +0000

    Refactor to use op graph + fixes; inputs/output
    Renames querytest to graph-query and changes it to use the OpGraph. A
    hack was added to deal with distributed count for the time being as the
    tables are not synchronized. Some new functions were added to
    QueryServer and Table to facilitate this change.
    
    Updates the test inputs to v4: some queries had to be changed binding
    the variable from a path to a vertex because the OpGraph (and
    technically even the older execution engine) did not support return
    variables bound to paths, and this was causing tests to fail. v4 has
    removed the queries and verify file: instead, they have been moved to
    the graph-query directory so that a small change does not require a
    completely new inputs tar to be created.
    
    [REWRITTEN COMMIT: incomplete subtree]

--- python/galois/example_utils.py
+++ python/galois/example_utils.py
@@ -20,7 +20,7 @@ def get_inputs_directory():
     if os.path.isdir(inputs_dir) and os.path.isfile(inputs_dir + "/propertygraphs/ldbc_003/meta"):
         return inputs_dir
     fn, _headers = urllib.request.urlretrieve(
-        "https://katana-ci-public.s3.us-east-1.amazonaws.com/inputs/katana-inputs-v3.tar.gz"
+        "https://katana-ci-public.s3.us-east-1.amazonaws.com/inputs/katana-inputs-v4.tar.gz"
     )
     try:
         with tarfile.open(fn) as tar:

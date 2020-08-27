8cf9eb193aad31565f2d573ed1c09cb940679a96
Arthur Peters <amp@singingwizard.org>
2020-08-27 15:25:52 -0500

    python: Make PropertyGraph.get_*_property return Array if possible
    This provides nearly 10x performance improvement in some cases.
    
    This slightly changes the type of get_{edge,node}_property to
    return an Array if the chunked array of the property only has one
    chunk (the chunked array is returned otherwise). This is the most
    common path. The APIs of ChunkedArray and Array are similar enough
    that they are interchangable for most uses in python and in numba
    compiled code. This change adds get_{edge,node}_property_chunked
    to get the original chunked array if needed.
    
    The reason for this change is that accesses to chunked arrays are
    much slower due to the use of reference counted shared_ptrs and
    the need to iterate over the chunks in several cases.
    
    [REWRITTEN COMMIT: incomplete subtree]

--- tests/test_property_graph.py
+++ tests/test_property_graph.py
@@ -49,6 +49,14 @@ def test_get_node_property(property_graph):
     assert prop1 == prop2
 
 
+def test_get_node_property_chunked(property_graph):
+    prop1 = property_graph.get_node_property(4)
+    assert isinstance(prop1, pyarrow.Array)
+    prop2 = property_graph.get_node_property_chunked(4)
+    assert isinstance(prop2, pyarrow.ChunkedArray)
+    assert prop1 == prop2.chunk(0)
+
+
 def test_remove_node_property(property_graph):
     property_graph.remove_node_property(10)
     assert len(property_graph.node_schema()) == 31
@@ -68,7 +76,10 @@ def test_add_node_property(property_graph):
     t = pyarrow.table(dict(new_prop=range(property_graph.num_nodes())))
     property_graph.add_node_property(t)
     assert len(property_graph.node_schema()) == 33
-    assert property_graph.get_node_property("new_prop") == pyarrow.chunked_array([range(property_graph.num_nodes())])
+    assert property_graph.get_node_property_chunked("new_prop") == pyarrow.chunked_array(
+        [range(property_graph.num_nodes())]
+    )
+    assert property_graph.get_node_property("new_prop") == pyarrow.array(range(property_graph.num_nodes()))
 
 
 def test_get_edge_property(property_graph):
@@ -76,6 +87,14 @@ def test_get_edge_property(property_graph):
     assert prop1[10].as_py() == False
     prop2 = property_graph.get_edge_property("IS_SUBCLASS_OF")
     assert prop1 == prop2
+
+
+def test_get_edge_property_chunked(property_graph):
+    prop1 = property_graph.get_edge_property(5)
+    assert isinstance(prop1, pyarrow.Array)
+    prop2 = property_graph.get_edge_property_chunked(5)
+    assert isinstance(prop2, pyarrow.ChunkedArray)
+    assert prop1 == prop2.chunk(0)
 
 
 def test_remove_edge_property(property_graph):
@@ -97,7 +116,10 @@ def test_add_edge_property(property_graph):
     t = pyarrow.table(dict(new_prop=range(property_graph.num_edges())))
     property_graph.add_edge_property(t)
     assert len(property_graph.edge_schema()) == 20
-    assert property_graph.get_edge_property("new_prop") == pyarrow.chunked_array([range(property_graph.num_edges())])
+    assert property_graph.get_edge_property_chunked("new_prop") == pyarrow.chunked_array(
+        [range(property_graph.num_edges())]
+    )
+    assert property_graph.get_edge_property("new_prop") == pyarrow.array(range(property_graph.num_edges()))
 
 
 def test_load_invalid_path():

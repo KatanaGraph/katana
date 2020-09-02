e5b79c54ad7eef643abe82f9266b21066ab3cb17
Arthur Peters <amp@singingwizard.org>
2020-09-02 15:29:06 -0500

    python: Remove user-facing Cython dependencies, tools, and tests.
    This replaces the jupyter-cython test with a jupyter-numba test.
    
    [REWRITTEN COMMIT: incomplete subtree]

--- python/conda_recipe/run_test.sh
+++ python/conda_recipe/run_test.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Testing Jupyter..."
-jupyter nbconvert --execute --to markdown test_notebook.ipynb
+jupyter nbconvert --execute --to markdown python/examples/jupyter/jaccard_numba.ipynb
 
 echo "Testing with pytest..."
 pytest -v

8d28b65b65ee36a3fd006cc04d6513170567f41f
Daniel Mawhirter <danielmawhirter@users.noreply.github.com>
2020-10-02 18:39:08 -0500

    Full-Table Result Checking (#517)
    * graph-query outputs a json containing the whole-table result for each query, count() no longer expected
    
    * json results checked against neo4j result
    
    * updated queries, added distributed, still need to modify inputs.tar.gz in s3 with canonical results
    
    * input version bump, cleanup of json result files
    
    * namespace on JsonDump
    
    * modified TODO
    
    * added more tests to json suite
    
    * some dist tests were failing, removed for now
    
    * format
    
    * reconfigured tests to avoid filename collision
    
    * reconfigured tests to avoid filename collision
    
    * more places for the input version bump
    
    * Update json_checker.py
    
    * hopefully the last time messing with paths
    
    * moved dist json tests to failing
    
    * re-enable dist tests
    
    * don't do 0-size append
    
    * format
    
    * :/
    
    [REWRITTEN COMMIT: incomplete subtree]

---
+++ scripts/json_checker.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+#
+# Script to check the output of algorithms:
+# Author: Daniel Mawhirter (daniel@mawhirter.com)
+#
+# Expects json files, intended for ordered results from querying
+
+import argparse
+import os
+import json
+
+def main(resultjson, truthdir):
+  failures = 0
+  successes = 0
+  with open(resultjson) as results_file:
+    results = json.load(results_file)
+    for query in results:
+      with open(truthdir + "/" + query + ".truth") as truth_file:
+        truth = json.load(truth_file)
+        if results[query] != truth:
+          print('Error at', query)
+          failures += 1
+        else:
+          successes += 1
+  if failures > 0:
+    print("\nFAILED (", failures, "/", (successes+failures), ")\n")
+  else:
+    print("\nSUCCESS (", successes, ")\n")
+  return failures
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Check graph output results as json")
+
+    parser.add_argument(
+        "-delete",
+        "-d",
+        type=bool,
+        nargs=1,
+        default=False,
+        help="delete the generated output files",
+    )
+    parser.add_argument(
+        "-resultjson",
+        help="file containing json of results"
+    )
+    parser.add_argument(
+        "-truthdir",
+        help="directory containing json files of ground truth"
+    )
+
+    parsed_arguments = parser.parse_args()
+
+    print("Starting comparison...")
+    ret = main(parsed_arguments.resultjson, parsed_arguments.truthdir)
+    if parsed_arguments.delete:
+        os.remove(parsed_arguments.resultjson)
+        countfile = parsed_arguments.resultjson.replace('.json', '.count')
+        os.remove(countfile)
+
+    if ret:
+        sys.exit(1)

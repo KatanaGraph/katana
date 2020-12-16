e3e6c88c3e15337b0d2e540cb816b89032758055
Daniel Mawhirter <danielmawhirter@users.noreply.github.com>
2020-12-16 13:52:11 -0600

    NULL Support (#683)
    The Partitioner, as well as Project and OrderBy. now maintain null values when sending over MPI. Analytics applications tolerate this through the Properties.h api, which interprets nulls as default-initialized values, as those applications expect. 
    
    This PR also builds on the API for accessing values from nullable arrays in querying. These APIs should fail on asserts whenever the semantics are incorrect, rather than silently producing incorrect results.
    
    LDBC IC8 and IC11 now work on shared memory and distributed, IC2 works on shared memory.
    
    [REWRITTEN COMMIT: self-contained subtree]

--- libgalois/include/galois/ArrowRandomAccessBuilder.h
+++ libgalois/include/galois/ArrowRandomAccessBuilder.h
@@ -25,8 +25,10 @@ public:
 
   reference operator[](size_t index) {
     assert(index < size());
-    return ((ValueType*)data_.data())[index];
+    return static_cast<ValueType*>(data_.data())[index];
   }
+
+  bool IsValid(size_t) { return true; }
 
   size_t size() const { return data_.size(); }
 
@@ -51,36 +53,108 @@ private:
   std::vector<StorageType> data_;
 };
 
-template <typename Property>
-struct PropertyTypeConfig;
+/// NullableBuilder uses std::vector for storage
+/// Finalize() makes a copy of the data
+/// Supports null values
+template <typename ValueType, typename StorageType, typename ArrowType>
+class NullableBuilder {
+public:
+  using value_type = ValueType;
+  using reference = ValueType&;
 
-#define NONULL(ValueType, StorageType, PropertyType)                           \
-  template <>                                                                  \
-  struct PropertyTypeConfig<PropertyType> {                                    \
-    using BuilderType = NoNullBuilder<                                         \
-        ValueType, StorageType, typename PropertyType::ArrowType>;             \
+  NullableBuilder(size_t length) : data_(length), valid_(length, false) {
+    static_assert(sizeof(ValueType) == sizeof(StorageType));
   }
 
-NONULL(uint8_t, uint8_t, galois::UInt8Property);
-NONULL(uint16_t, uint16_t, galois::UInt16Property);
-NONULL(uint32_t, uint32_t, galois::UInt32Property);
-NONULL(uint64_t, uint64_t, galois::UInt64Property);
-NONULL(uint8_t, uint8_t, galois::BooleanReadOnlyProperty);
-NONULL(std::string, std::string, galois::StringReadOnlyProperty);
-NONULL(std::string, std::string, galois::LargeStringReadOnlyProperty);
+  // NOTE this operator has side-effects. It can safely be used in two ways:
+  // 1) builder[index] = value; where it creates a non-null entry
+  // 2) value = builder[index]; ONLY IF option 1 has already used that index
+  reference operator[](size_t index) {
+    assert(index < size());
+    valid_[index] = true;
+    return reinterpret_cast<ValueType*>(data_.data())[index];
+  }
 
-#undef NONULL
+  bool IsValid(size_t index) { return valid_[index]; }
+
+  size_t size() const { return data_.size(); }
+
+  galois::Result<void> Finalize(std::shared_ptr<arrow::Array>* array) const {
+    using ArrowBuilder = typename arrow::TypeTraits<ArrowType>::BuilderType;
+    ArrowBuilder builder;
+    if (data_.size() > 0) {
+      if constexpr (std::is_scalar_v<value_type>) {
+        // TODO(danielmawhirter) find a better way to handle this
+        // arrow::NumericBuilder has:
+        // AppendValues(value_type*, int64_t, uint8_t*)
+        // AppendValues(value_type*, int64_t, vector<bool>)
+        // AppendValues(vector<value_type>, vector<bool>)
+        if (auto r =
+                builder.AppendValues(data_.data(), data_.size(), valid_.data());
+            !r.ok()) {
+          GALOIS_LOG_DEBUG("arrow error: {}", r);
+          return galois::ErrorCode::ArrowError;
+        }
+      } else {
+        // TODO(danielmawhirter) find a better way to handle this
+        // arrow::BinaryBuilder has:
+        // AppendValues(vector<string>, uint8_t*)
+        // AppendValues(char**, int64_t, uint8_t*)
+        if (auto r = builder.AppendValues(data_, valid_.data()); !r.ok()) {
+          GALOIS_LOG_DEBUG("arrow error: {}", r);
+          return galois::ErrorCode::ArrowError;
+        }
+      }
+    }
+    if (auto r = builder.Finish(array); !r.ok()) {
+      GALOIS_LOG_DEBUG("arrow error: {}", r);
+      return galois::ErrorCode::ArrowError;
+    }
+    return galois::ResultSuccess();
+  }
+
+private:
+  std::vector<StorageType> data_;
+  std::vector<uint8_t> valid_;
+};
+
+template <typename ArrowType>
+struct ArrowTypeConfig;
+
+#define NULLABLE(ValueType, StorageType, ArrowType)                            \
+  template <>                                                                  \
+  struct ArrowTypeConfig<ArrowType> {                                          \
+    using RandomBuilderType =                                                  \
+        NullableBuilder<ValueType, StorageType, ArrowType>;                    \
+  }
+
+NULLABLE(int8_t, int8_t, arrow::Int8Type);
+NULLABLE(uint8_t, uint8_t, arrow::UInt8Type);
+NULLABLE(int16_t, int16_t, arrow::Int16Type);
+NULLABLE(uint16_t, uint16_t, arrow::UInt16Type);
+NULLABLE(int32_t, int32_t, arrow::Int32Type);
+NULLABLE(uint32_t, uint32_t, arrow::UInt32Type);
+NULLABLE(int64_t, int64_t, arrow::Int64Type);
+NULLABLE(uint64_t, uint64_t, arrow::UInt64Type);
+NULLABLE(float, float, arrow::FloatType);
+NULLABLE(double, double, arrow::DoubleType);
+NULLABLE(bool, uint8_t, arrow::BooleanType);
+NULLABLE(std::string, std::string, arrow::StringType);
+NULLABLE(std::string, std::string, arrow::LargeStringType);
+
+#undef NULLABLE
 
 }  // namespace
 
 /// ArrowRandomAccessBuilder encapsulates the concept of building
 /// an arrow::Array from <index, value> pairs arriving in unknown order
-/// Functions as a wrapper for NoNullBuilder currently, TODO(danielmawhirter)
-template <typename Property>
+/// Functions as a wrapper for NullableBuilder currently, TODO(danielmawhirter)
+template <typename ArrowType>
 class ArrowRandomAccessBuilder {
 public:
-  using BuilderType = typename PropertyTypeConfig<Property>::BuilderType;
-  using value_type = typename BuilderType::value_type;
+  using RandomBuilderType =
+      typename ArrowTypeConfig<ArrowType>::RandomBuilderType;
+  using value_type = typename RandomBuilderType::value_type;
 
   ArrowRandomAccessBuilder(size_t length) : builder_(length) {}
 
@@ -90,14 +164,25 @@ public:
 
   value_type& operator[](size_t index) { return builder_[index]; }
 
+  bool IsValid(size_t index) { return builder_.IsValid(index); }
+
+  galois::Result<void> Finalize(std::shared_ptr<arrow::Array>* array) {
+    return builder_.Finalize(array);
+  }
+
   galois::Result<std::shared_ptr<arrow::Array>> Finalize() {
-    return builder_.Finalize();
+    std::shared_ptr<arrow::Array> array;
+    auto res = Finalize(&array);
+    if (!res) {
+      return res.error();
+    }
+    return array;
   }
 
   size_t size() const { return builder_.size(); }
 
 private:
-  BuilderType builder_;
+  RandomBuilderType builder_;
 };
 
 }  // namespace galois

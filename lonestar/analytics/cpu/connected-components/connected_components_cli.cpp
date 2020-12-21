a8f7c291ef2f0873f4b89a73927b73ed92d44ff6
Gurbinder Gill <gill@cs.utexas.edu>
2020-12-21 00:27:00 -0600

    Add connected-components to analytics library

---
+++ lonestar/analytics/cpu/connected-components/connected_components_cli.cpp
@@ -0,0 +1,247 @@
+/*
+ * This file belongs to the Galois project, a C++ library for exploiting
+ * parallelism. The code is being released under the terms of the 3-Clause BSD
+ * License (a copy is located in LICENSE.txt at the top-level directory).
+ *
+ * Copyright (C) 2018, The University of Texas at Austin. All rights reserved.
+ * UNIVERSITY EXPRESSLY DISCLAIMS ANY AND ALL WARRANTIES CONCERNING THIS
+ * SOFTWARE AND DOCUMENTATION, INCLUDING ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR ANY PARTICULAR PURPOSE, NON-INFRINGEMENT AND WARRANTIES OF
+ * PERFORMANCE, AND ANY WARRANTY THAT MIGHT OTHERWISE ARISE FROM COURSE OF
+ * DEALING OR USAGE OF TRADE.  NO WARRANTY IS EITHER EXPRESS OR IMPLIED WITH
+ * RESPECT TO THE USE OF THE SOFTWARE OR DOCUMENTATION. Under no circumstances
+ * shall University be liable for incidental, special, indirect, direct or
+ * consequential damages or loss of profits, interruption of business, or
+ * related expenses which may arise from use of Software or Documentation,
+ * including but not limited to those resulting from defects in Software and/or
+ * Documentation, or loss or inaccuracy of data of any kind.
+ */
+
+#include <iostream>
+
+#include "Lonestar/BoilerPlate.h"
+#include "galois/analytics/connected_components/connected_components.h"
+
+using namespace galois::analytics;
+
+namespace cll = llvm::cl;
+
+const char* name = "Connected Components";
+const char* desc = "Computes the connected components of a graph";
+static const char* url = "connected_components";
+
+static cll::opt<std::string> inputFile(
+    cll::Positional, cll::desc("<input file>"), cll::Required);
+static cll::opt<unsigned int> reportNode(
+    "reportNode", cll::desc("Node to report distance to(default value 1)"),
+    cll::init(1));
+
+static cll::opt<ConnectedComponentsPlan::Algorithm> algo(
+    "algo", cll::desc("Choose an algorithm (default value Afforest):"),
+    cll::values(
+        clEnumValN(
+            ConnectedComponentsPlan::kSerial, "Serial", "Serial algorithm"),
+        clEnumValN(
+            ConnectedComponentsPlan::kLabelProp, "LabelProp",
+            "Label propagation algorithms"),
+        clEnumValN(
+            ConnectedComponentsPlan::kSynchronous, "Synchronous",
+            "Synchronous algorithm"),
+        clEnumValN(
+            ConnectedComponentsPlan::kAsynchronous, "Asynchronous",
+            "Asynchronous algorithm"),
+        clEnumValN(
+            ConnectedComponentsPlan::kEdgeAsynchronous, "EdgeAsynchronous",
+            "Edge asynchronous algorithm"),
+        clEnumValN(
+            ConnectedComponentsPlan::kEdgeTiledAsynchronous,
+            "EdgeTiledAsynchronous", "Edge tiled asynchronous algorithm"),
+        clEnumValN(
+            ConnectedComponentsPlan::kBlockedAsynchronous,
+            "BlockedASynchronous", "Blocked asynchronous algorithm"),
+        clEnumValN(
+            ConnectedComponentsPlan::kAfforest, "Afforest",
+            "Afforest sampling algorithm"),
+        clEnumValN(
+            ConnectedComponentsPlan::kEdgeAfforest, "EdgeAfforest",
+            "Afforest (edge-wise) sampling algorithm"),
+        clEnumValN(
+            ConnectedComponentsPlan::kEdgeTiledAfforest, "EdgeTiledAfforest",
+            "Afforest (tiled edge-wise) sampling algorithm")),
+    cll::init(ConnectedComponentsPlan::kAfforest));
+
+static cll::opt<uint32_t> edgeTileSize(
+    "edgeTileSize",
+    cll::desc("(For Edgetiled algos) Size of edge tiles "
+              "(default 512)"),
+    cll::init(512));
+//! parameter for the Vertex Neighbor Sampling step of Afforest algorithm
+static cll::opt<uint32_t> neighborSampleSize(
+    "neighborSampleSize",
+    cll::desc("(For Afforest and its variants) number of edges "
+              "per vertice to process initially for exposing "
+              "partial connectivity (default 2)"),
+    cll::init(2));
+//! parameter for the Large Component Skipping step of Afforest algorithm
+static cll::opt<uint32_t> componentSampleFrequency(
+    "componentSampleFrequency",
+    cll::desc("(For Afforest and its variants) number of times "
+              "randomly sampling over vertices to approximately "
+              "capture the largest intermediate component "
+              "(default 1024)"),
+    cll::init(1024));
+
+std::string
+AlgorithmName(ConnectedComponentsPlan::Algorithm algorithm) {
+  switch (algorithm) {
+  case ConnectedComponentsPlan::kSerial:
+    return "Serial";
+  case ConnectedComponentsPlan::kLabelProp:
+    return "LabelProp";
+  case ConnectedComponentsPlan::kSynchronous:
+    return "Synchronous";
+  case ConnectedComponentsPlan::kAsynchronous:
+    return "Asynchronous";
+  case ConnectedComponentsPlan::kEdgeAsynchronous:
+    return "EdgeAsynchronous";
+  case ConnectedComponentsPlan::kEdgeTiledAsynchronous:
+    return "EdgeTiledAsynchronous";
+  case ConnectedComponentsPlan::kBlockedAsynchronous:
+    return "BlockedAsynchronous";
+  case ConnectedComponentsPlan::kAfforest:
+    return "Afforest";
+  case ConnectedComponentsPlan::kEdgeAfforest:
+    return "EdgeAfforest";
+  case ConnectedComponentsPlan::kEdgeTiledAfforest:
+    return "EdgeTiledAfforest";
+  default:
+    return "Unknown";
+  }
+}
+
+int
+main(int argc, char** argv) {
+  std::unique_ptr<galois::SharedMemSys> G =
+      LonestarStart(argc, argv, name, desc, url, &inputFile);
+
+  galois::StatTimer totalTime("TimerTotal");
+  totalTime.start();
+
+  if (!symmetricGraph) {
+    GALOIS_LOG_FATAL(
+        "This application requires a symmetric graph input;"
+        " please use the -symmetricGraph flag "
+        " to indicate the input is a symmetric graph.");
+  }
+
+  std::cout << "Reading from file: " << inputFile << "\n";
+  std::unique_ptr<galois::graphs::PropertyFileGraph> pfg =
+      MakeFileGraph(inputFile, edge_property_name);
+
+  std::cout << "Read " << pfg->topology().num_nodes() << " nodes, "
+            << pfg->topology().num_edges() << " edges\n";
+
+  if (reportNode >= pfg->topology().num_nodes()) {
+    std::cerr << "failed to set report: " << reportNode << "\n";
+    abort();
+  }
+
+  galois::reportPageAlloc("MeminfoPre");
+
+  std::cout << "Running " << AlgorithmName(algo) << " algorithm\n";
+
+  ConnectedComponentsPlan plan = ConnectedComponentsPlan();
+  switch (algo) {
+  case ConnectedComponentsPlan::kSerial:
+    plan = ConnectedComponentsPlan::Serial();
+    break;
+  case ConnectedComponentsPlan::kLabelProp:
+    plan = ConnectedComponentsPlan::LabelProp();
+    break;
+  case ConnectedComponentsPlan::kSynchronous:
+    plan = ConnectedComponentsPlan::Synchronous();
+    break;
+  case ConnectedComponentsPlan::kAsynchronous:
+    plan = ConnectedComponentsPlan::Asynchronous();
+    break;
+  case ConnectedComponentsPlan::kEdgeAsynchronous:
+    plan = ConnectedComponentsPlan::EdgeAsynchronous();
+    break;
+  case ConnectedComponentsPlan::kEdgeTiledAsynchronous:
+    galois::gInfo("INFO: Using edge tile size: ", edgeTileSize);
+    galois::gInfo("WARNING: Performance may vary due to parameter");
+    plan = ConnectedComponentsPlan::EdgeTiledAsynchronous(edgeTileSize);
+    break;
+  case ConnectedComponentsPlan::kBlockedAsynchronous:
+    plan = ConnectedComponentsPlan::BlockedAsynchronous();
+    break;
+  case ConnectedComponentsPlan::kAfforest:
+    galois::gInfo(
+        "INFO: Using neighbor sample size: ", neighborSampleSize,
+        " component sample frequency: ", componentSampleFrequency);
+    galois::gInfo("WARNING: Performance may vary due to the parameters");
+    plan = ConnectedComponentsPlan::Afforest(
+        neighborSampleSize, componentSampleFrequency);
+    break;
+  case ConnectedComponentsPlan::kEdgeAfforest:
+    galois::gInfo(
+        "INFO: Using neighbor sample size: ", neighborSampleSize,
+        " component sample frequency: ", componentSampleFrequency);
+    galois::gInfo("WARNING: Performance may vary due to the parameters");
+    plan = ConnectedComponentsPlan::EdgeAfforest(
+        neighborSampleSize, componentSampleFrequency);
+    break;
+  case ConnectedComponentsPlan::kEdgeTiledAfforest:
+    galois::gInfo(
+        "INFO: Using edge tile size: ", edgeTileSize,
+        " neighbor sample size: ", neighborSampleSize,
+        " component sample frequency: ", componentSampleFrequency);
+    galois::gInfo("WARNING: Performance may vary due to the parameters");
+    plan = ConnectedComponentsPlan::EdgeTiledAfforest(
+        neighborSampleSize, componentSampleFrequency);
+    break;
+  default:
+    std::cerr << "Invalid algorithm\n";
+    abort();
+  }
+
+  auto pg_result = ConnectedComponents(pfg.get(), "component", plan);
+  if (!pg_result) {
+    GALOIS_LOG_FATAL(
+        "Failed to run ConnectedComponents: {}", pg_result.error());
+  }
+
+  auto stats_result =
+      ConnectedComponentsStatistics::Compute(pfg.get(), "component");
+  if (!stats_result) {
+    GALOIS_LOG_FATAL(
+        "Failed to compute ConnectedComponents statistics: {}",
+        stats_result.error());
+  }
+  auto stats = stats_result.value();
+  stats.Print();
+
+  if (!skipVerify) {
+    if (auto r = ConnectedComponentsValidate(pfg.get(), "component");
+        r && r.value()) {
+      std::cout << "Verification successful.\n";
+    } else {
+      GALOIS_LOG_FATAL("verification failed");
+    }
+  }
+
+  if (output) {
+    auto r = pfg->NodePropertyTyped<uint64_t>("component");
+    if (!r) {
+      GALOIS_LOG_FATAL("Failed to get node property {}", stats_result.error());
+    }
+    auto results = r.value();
+    assert(uint64_t(results->length()) == pfg->topology().num_nodes());
+
+    writeOutput(outputLocation, results->raw_values(), results->length());
+  }
+
+  totalTime.stop();
+
+  return 0;
+}

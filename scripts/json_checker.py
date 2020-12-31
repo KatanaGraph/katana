9d2539a9d12efe724e8545c6f4695a98e91c84c1
danielmawhirter <daniel@mawhirter.com>
2020-12-31 20:02:15 +0000

    Clarify output of jeon-checker, update for new output path approach

--- scripts/json_checker.py
+++ scripts/json_checker.py
@@ -17,8 +17,18 @@ def compare(resultfile, truthfile):
   try:
     with open(resultfile) as result:
       result_dicts = [ json.loads(line.strip()) for line in result ]
+  except FileNotFoundError:
+    logging.error("missing result file %s", resultfile)
+    return False
+
+  try:
     with open(truthfile) as truth:
       truth_dicts = [ json.loads(line.strip()) for line in truth ]
+  except FileNotFoundError:
+    logging.error("missing truth file %s", truthfile)
+    return False
+
+  try:
     if len(result_dicts) != len(truth_dicts):
       logging.error("length mismatch: result=%s truth=%s", len(result_dicts), len(truth_dicts))
       correct = False
@@ -29,12 +39,11 @@ def compare(resultfile, truthfile):
         break
     if not correct:
       left = [ dict(sorted(a.items())) for a in result_dicts if a not in truth_dicts ]
-      logging.error("only in results: %s of %s items (first 10):\n%s", len(left), len(result_dicts), '\n'.join([ str(x) for x,_ in zip(left, range(10)) ]))
+      logging.error("only in results: %s of %s items (first 10):\n%s", len(left), len(result_dicts),
+                    '\n'.join([ str(x) for x,_ in zip(left, range(10)) ]))
       right = [ dict(sorted(b.items())) for b in truth_dicts if b not in result_dicts ]
-      logging.error("only in truth: %s of %s items (first 10):\n%s", len(right), len(truth_dicts), '\n'.join([ str(x) for x,_ in zip(right, range(10)) ]))
-  except FileNotFoundError:
-    logging.error("missing either %s or %s", resultfile, truthfile)
-    correct = False
+      logging.error("only in truth: %s of %s items (first 10):\n%s", len(right), len(truth_dicts),
+                    '\n'.join([ str(x) for x,_ in zip(right, range(10)) ]))
   except Exception as e:
     logging.error("exception: %s", str(e))
     traceback.print_exc()
@@ -42,7 +51,7 @@ def compare(resultfile, truthfile):
   finally:
     return correct
 
-def main(querylist, resultprefix, truthdir, delete):
+def main(querylist, resultdir, truthdir, delete):
   successes = 0
   failures = 0
   with open(querylist) as queries_file:
@@ -52,12 +61,12 @@ def main(querylist, resultprefix, truthdir, delete):
       query_args = line.strip().split()
       query_name = query_args[0]
       if len(query_args) == 1:
-        query_filename = resultprefix + "_" + query_name + ".json"
-        truth_filename = truthdir + "/" + query_name + ".truth"
+        query_filename = os.path.join(resultdir, query_name + ".json")
+        truth_filename = os.path.join(truthdir, query_name + ".truth_json")
       elif len(query_args) == 2:
         param_name = query_args[1]
-        query_filename = resultprefix + "_" + query_name + "_" + param_name + ".json"
-        truth_filename = truthdir + "/" + query_name + "_" + param_name + ".truth"
+        query_filename = os.path.join(resultdir, query_name + "_" + param_name + ".json")
+        truth_filename = os.path.join(truthdir, query_name + "_" + param_name + ".truth_json")
       else:
         print("query args unclear:", query_args)
         return 1
@@ -92,7 +101,7 @@ if __name__ == "__main__":
         help="list of queries being tested"
     )
     parser.add_argument(
-        "-resultprefix",
+        "-resultdir",
         help="prefix for files containing json of results"
     )
     parser.add_argument(
@@ -103,10 +112,10 @@ if __name__ == "__main__":
     parsed_arguments = parser.parse_args()
 
     print("Starting comparison...")
-    ret = main(parsed_arguments.querylist, parsed_arguments.resultprefix,
+    ret = main(parsed_arguments.querylist, parsed_arguments.resultdir,
                parsed_arguments.truthdir, parsed_arguments.delete)
     if parsed_arguments.delete:
-        countfile = parsed_arguments.resultprefix + ".count"
+        countfile = parsed_arguments.resultdir + ".count"
         if os.path.exists(countfile):
           os.remove(countfile)
 

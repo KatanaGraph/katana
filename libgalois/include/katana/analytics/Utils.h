95ac13cb34c2bcde738b1395434dc5f04c05a26e
Arthur Peters <amp@katanagraph.com>
2021-01-07 22:35:04 -0600

    Refactor isApproximateDegreeDistributionPowerLaw.
    Use PropertyFileGraph.

--- libgalois/include/katana/analytics/Utils.h
+++ libgalois/include/katana/analytics/Utils.h
@@ -10,60 +10,27 @@
 
 namespace katana::analytics {
 
+// TODO(amp): This file should be disbanded and it's functions moved to
+//  PropertyFileGraph.h or other more specific places.
+
 //! Used to pick random non-zero degree starting points for search algorithms
 //! This code has been copied from GAP benchmark suite
 //! (https://github.com/sbeamer/gapbs/blob/master/src/benchmark.h)
-template <typename Graph>
-class SourcePicker {
-  static const uint32_t kRandSeed;
-  std::mt19937 rng;
-  std::uniform_int_distribution<typename Graph::Node> udist;
-  const Graph& graph;
+class KATANA_EXPORT SourcePicker {
+  const PropertyFileGraph& graph;
 
 public:
-  explicit SourcePicker(const Graph& g)
-      : rng(kRandSeed), udist(0, g.size() - 1), graph(g) {}
+  explicit SourcePicker(const PropertyFileGraph& g) : graph(g) {}
 
-  auto PickNext() {
-    typename Graph::Node source;
-    do {
-      source = udist(rng);
-    } while (
-        std::distance(graph.edges(source).begin(), graph.edges(source).end()));
-    return source;
-  }
+  uint32_t PickNext();
 };
-
-template <typename Graph>
-const uint32_t SourcePicker<Graph>::kRandSeed = 27491095;
 
 //! Used to determine if a graph has power-law degree distribution or not
 //! by sampling some of the vertices in the graph randomly
 //! This code has been copied from GAP benchmark suite
 //! (https://github.com/sbeamer/gapbs/blob/master/src/tc.cc WorthRelabelling())
-template <typename Graph>
-bool
-isApproximateDegreeDistributionPowerLaw(const Graph& graph) {
-  uint32_t averageDegree = graph.num_edges() / graph.num_nodes();
-  if (averageDegree < 10)
-    return false;
-  SourcePicker<Graph> sp(graph);
-  uint32_t num_samples = 1000;
-  if (num_samples > graph.size())
-    num_samples = graph.size();
-  uint32_t sample_total = 0;
-  std::vector<uint32_t> samples(num_samples);
-  for (uint32_t trial = 0; trial < num_samples; trial++) {
-    typename Graph::Node node = sp.PickNext();
-    samples[trial] =
-        std::distance(graph.edges(node).begin(), graph.edges(node).end());
-    sample_total += samples[trial];
-  }
-  std::sort(samples.begin(), samples.end());
-  double sample_average = static_cast<double>(sample_total) / num_samples;
-  double sample_median = samples[num_samples / 2];
-  return sample_average / 1.3 > sample_median;
-}
+KATANA_EXPORT bool IsApproximateDegreeDistributionPowerLaw(
+    const PropertyFileGraph& graph);
 
 template <typename Props>
 std::vector<std::string>
@@ -80,10 +47,9 @@ DefaultPropertyNames() {
 template <typename NodeProps>
 inline katana::Result<void>
 ConstructNodeProperties(
-    katana::PropertyFileGraph* pfg,
+    PropertyFileGraph* pfg,
     const std::vector<std::string>& names = DefaultPropertyNames<NodeProps>()) {
-  auto res_table =
-      katana::AllocateTable<NodeProps>(pfg->topology().num_nodes(), names);
+  auto res_table = katana::AllocateTable<NodeProps>(pfg->num_nodes(), names);
   if (!res_table) {
     return res_table.error();
   }
@@ -94,7 +60,7 @@ ConstructNodeProperties(
 template <typename EdgeProps>
 inline katana::Result<void>
 ConstructEdgeProperties(
-    katana::PropertyFileGraph* pfg,
+    PropertyFileGraph* pfg,
     const std::vector<std::string>& names = DefaultPropertyNames<EdgeProps>()) {
   auto res_table =
       katana::AllocateTable<EdgeProps>(pfg->topology().num_edges(), names);

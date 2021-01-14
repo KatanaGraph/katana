904f5f974942d1dcd7adac82ca927610c2752338
pamvazquez <pamvazquez@daito.katanagraph.com>
2021-01-14 23:49:07 +0000

    Added a summary of the algorithm and checked for syntax errors

--- python/galois/lonestar/analytics/average_degree_connectivity.py
+++ python/galois/lonestar/analytics/average_degree_connectivity.py
@@ -6,6 +6,27 @@ from galois.loops import do_all, do_all_operator
 from galois.property_graph import PropertyGraph
 from galois.shmem import setActiveThreads
 
+
+#SUMMARY OF THE ALGORITHM: 
+##the average degree connectivity of a degree in graph g is the sum 
+#of the average neighbor degree for nodes with degree k divided by 
+#the number of nodes with that degree
+
+#Thus the first step is to create two dictionaries that will hold all
+#of the in/out degrees seen in graph g these are name in_dict and out_dict
+#where key = degree and value = array of node_id's with that degree 
+
+#Second step is to have a method that calculate the sume of the average
+#neighbor degree for nodes with degree k
+
+#Thus when we call get_avg_degconn, we can use our in_dict and out_dict 
+#to go through every degree in the dictionary and calculate the 
+#average degree connectivity for degree k by calling 
+# avg_n_sum / value of key (degree) which the number of nodes with that degree
+#We store this in a result dictionary where
+#key = degree and value = degree's average degree connectivity
+
+
 #QUESTIONS: ask about correct way to pass parameters :) example: line 76
 #TODO: fix indentation
 
@@ -91,8 +112,12 @@ def get_avg_degconn(graph: PropertyGraph, source, target, nodes, weight):
 
         for degree in in_dict.keys(): 
 
-            #get value (array of NID's)
-            value = inDict.get(degree)
+            #get value (array of NID's) that have that degree
+            value = in_dict.get(degree)
+
+            #the average degree connectivity of a degree in graph g is the sum 
+            #of the average neighbor degree for nodes with degree k divided by 
+            #the number of nodes with that degree
             avg_degree_connectivity = avg_n_sum(value) / len(value)
             
             #add key value pair

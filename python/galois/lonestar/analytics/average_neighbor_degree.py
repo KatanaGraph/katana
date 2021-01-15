378e68af78e9c6e898617cdc4eb0e7c86b65659d
pamvazquez <pamvazquez@daito.katanagraph.com>
2021-01-15 18:33:48 +0000

    Added most updated version of average_neighbor_degree

--- python/galois/lonestar/analytics/average_neighbor_degree.py
+++ python/galois/lonestar/analytics/average_neighbor_degree.py
@@ -7,13 +7,30 @@ from galois.property_graph import PropertyGraph
 from galois.shmem import setActiveThreads
 from .deg_count import calculate_degree
 
+
+#ALGORITHM SUMMARY: the algorithm returns the average_neighbor_degree of each 
+#node in the graph
+#We attain the average_neighbor_degree in two different ways, depending on 
+#if the edges are weighted or not
+#if the edges are NOT weighted, then we attain the average_neighbor_degree by
+#summing the degrees of node k's neighbor and dividing that by the neighbors
+#of node k
+#if the edges ARE weighted, then we attain the average_neighbor_degree by 
+#summing the degrees of node k's neighbors times the weight of the edge that
+#respectively connects the two nodes, all of that divided by the weighted degree
+#of node k 
+
+#Most recent error when running 
+#avg_n_deg.average_neighbor_degree(g, source="in", target="in", weight=None, weight_property=None)
+# Unknown attribute 'get_edge_property' of type PropertyGraph in
+# else: 
+    #weight_array = graph.get_edge_property(weight_property)
 #QUESTIONS: what is the legal way to get value from certain index of an array in pyarrow?
-#           what is the legal way to pass parameters? 
 
 
 ##method that sums up the nonweighted or weighted degrees of the current node's neighbors
 @do_all_operator()
-def sum_neighbor_degree(graph: PropertyGraph, result_array, deg_array, weight, weight_array, nid):
+def sum_neighbor_degree(graph: PropertyGraph, result_array, deg_array, weight, weight_property, nid):
     # fill in map that will hold the results key = node id and value = avg_neighbor_deg
     sum_neighbor_degree = 0
     #for edge connected to curr node: 
@@ -25,6 +42,7 @@ def sum_neighbor_degree(graph: PropertyGraph, result_array, deg_array, weight, w
             sum_neighbor_degree += deg_of_dst_node
 
         else: 
+            weight_array = graph.get_edge_property(weight_property)
             weight_of_edge = weight_array[edge]
             sum_neighbor_degree += (deg_of_dst_node * weight_of_edge)
 
@@ -34,7 +52,7 @@ def sum_neighbor_degree(graph: PropertyGraph, result_array, deg_array, weight, w
     result_array[nid] = avg_neighbor_degree
 
 #helper method that fills the result dictionary where key = nid and value = its average neighbor degree 
-def helper(graph: PropertyGraph, deg_array, weight, weight_property=None):
+def helper(graph: PropertyGraph, deg_array, weight, weight_property):
     # create map that will hold the results key = node id and value = avg_neighbor_deg
     #result_dict= dict()
 
@@ -46,7 +64,7 @@ def helper(graph: PropertyGraph, deg_array, weight, weight_property=None):
     do_all(
         range(num_nodes),
         #taking out nid parameter
-        sum_neighbor_degree(graph, result_array, deg_array, weight, graph.get_edge_property(weight_property)),
+        sum_neighbor_degree(graph, result_array, deg_array, weight, weight_property),
 
         #I think I dont necessarily need this here
         steal=True,
@@ -56,7 +74,7 @@ def helper(graph: PropertyGraph, deg_array, weight, weight_property=None):
 
 
 
-def average_neighbor_degree(graph: PropertyGraph, source, target, weight):
+def average_neighbor_degree(graph: PropertyGraph, source, target, weight, weight_property=None):
    
     calculate_degree(graph, in_degree_property="in_degree_property", out_degree_property="out_degree_property", weight_property = weight)
 
@@ -65,7 +83,7 @@ def average_neighbor_degree(graph: PropertyGraph, source, target, weight):
     else: 
         deg_array = graph.get_node_property("out_degree_property")
 
-    result_array = helper(graph, deg_array, weight, weight_property=None)
+    result_array = helper(graph, deg_array, weight, weight_property)
 
     for node_id, avg_neighbor in enumerate(result_array):
         print(node_id, ' : ', avg_neighbor)

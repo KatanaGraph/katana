43f65ffd540bd61a381b33583453ab881479b239
Tyler Hunt <thunt@katanagraph.com>
2021-01-15 00:16:47 +0000

    support/strings: expose iterator API
    Signed-off-by: Tyler Hunt <thunt@katanagraph.com>

--- libsupport/include/katana/Strings.h
+++ libsupport/include/katana/Strings.h
@@ -33,17 +33,17 @@ KATANA_EXPORT std::string TrimSuffix(
 
 KATANA_EXPORT bool HasSuffix(const std::string& s, const std::string& suffix);
 
-/// Join returns a string that is the concatenation of every object in
-/// \param items all separated by an instance of \param sep
-template <typename C>
+/// Join returns a string that is the concatenation of every object from
+/// \param begin to \param end  all separated by an instance of \param sep
+template <typename I>
 std::string
-Join(std::string_view sep, const C& items) {
-  if (items.empty()) {
+Join(std::string_view sep, I begin, I end) {
+  if (begin == end) {
     return "";
   }
   fmt::memory_buffer buf;
-  auto last = --items.end();
-  for (auto it = items.begin(); it != last; ++it) {
+  auto last = --end;
+  for (auto it = begin; it != last; ++it) {
     fmt::format_to(buf, "{}{}", *it, sep);
   }
   fmt::format_to(buf, "{}", *last);
@@ -52,10 +52,18 @@ Join(std::string_view sep, const C& items) {
 
 /// Join returns a string that is the concatenation of every object in
 /// \param items all separated by an instance of \param sep
+template <typename C>
+std::string
+Join(std::string_view sep, const C& items) {
+  return Join(sep, items.begin(), items.end());
+}
+
+/// Join returns a string that is the concatenation of every object in
+/// \param items all separated by an instance of \param sep
 template <typename T>
 std::string
 Join(std::string_view sep, const std::initializer_list<T> items) {
-  return Join(sep, std::vector<T>(items));
+  return Join(sep, items.begin(), items.end());
 }
 
 }  // namespace katana

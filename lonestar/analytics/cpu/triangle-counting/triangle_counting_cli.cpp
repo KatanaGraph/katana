2bc933966c399ecdfcf3bde16950a43053b773e1
Arthur Peters <amp@katanagraph.com>
2021-01-15 16:59:49 -0600

    Wrap Triangle Count for C++ and Python.
    Also, remove docs reference to profiling in triangle count algo.

---
+++ lonestar/analytics/cpu/triangle-counting/triangle_counting_cli.cpp
@@ -0,0 +1,104 @@
+/*
+ * This file belongs to the Galois project, a C++ library for exploiting
+ * parallelism. The code is being released under the terms of the 3-Clause BSD
+ * License (a copy is located in LICENSE.txt at the top-level directory).
+ *
+ * Copyright (C) 2018, The University of Texas at Austin. All rights reserved.
+ * UNIVERSITY EXPRESSLY DISCLAIMS ANY AND ALL WARRANTIES CONCERNING THIS
+ * SOFTWARE AND DOCUMENTATION, INCLUDING ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR ANY PARTICULAR PURPOSE, NON-INFRINGEMENT AND WARRANTIES OF
+ * PERFORMANCE, AND ANY WARRANTY THAT MIGHT OTHERWISE ARISE FROM COURSE OF
+ * DEALING OR USAGE OF TRADE.  NO WARRANTY IS EITHER EXPRESS OR IMPLIED WITH
+ * RESPECT TO THE USE OF THE SOFTWARE OR DOCUMENTATION. Under no circumstances
+ * shall University be liable for incidental, special, indirect, direct or
+ * consequential damages or loss of profits, interruption of business, or
+ * related expenses which may arise from use of Software or Documentation,
+ * including but not limited to those resulting from defects in Software and/or
+ * Documentation, or loss or inaccuracy of data of any kind.
+ */
+
+#include <iostream>
+
+#include "Lonestar/BoilerPlate.h"
+#include "katana/analytics/triangle_count/triangle_count.h"
+
+using namespace katana::analytics;
+
+const char* name = "Triangles";
+const char* desc = "Counts the triangles in a graph";
+
+namespace cll = llvm::cl;
+
+static cll::opt<std::string> inputFile(
+    cll::Positional, cll::desc("<input file>"), cll::Required);
+static cll::opt<TriangleCountPlan::Algorithm> algo(
+    "algo", cll::desc("Choose an algorithm:"),
+    cll::values(
+        clEnumValN(
+            TriangleCountPlan::kNodeIteration, "nodeiterator", "Node Iterator"),
+        clEnumValN(
+            TriangleCountPlan::kEdgeIteration, "edgeiterator", "Edge Iterator"),
+        clEnumValN(
+            TriangleCountPlan::kOrderedCount, "orderedCount",
+            "Ordered Simple Count (default)")),
+    cll::init(TriangleCountPlan::kOrderedCount));
+
+static cll::opt<bool> relabel(
+    "relabel",
+    cll::desc("Relabel nodes of the graph (default value of false => "
+              "choose automatically)"),
+    cll::init(false));
+int
+main(int argc, char** argv) {
+  std::unique_ptr<katana::SharedMemSys> G =
+      LonestarStart(argc, argv, name, desc, nullptr, &inputFile);
+
+  katana::StatTimer totalTime("TimerTotal");
+  totalTime.start();
+
+  if (!symmetricGraph) {
+    KATANA_DIE(
+        "This application requires a symmetric graph input;"
+        " please use the -symmetricGraph flag "
+        " to indicate the input is a symmetric graph.");
+  }
+
+  std::cout << "Reading from file: " << inputFile << "\n";
+  std::unique_ptr<katana::PropertyFileGraph> pfg =
+      MakeFileGraph(inputFile, edge_property_name);
+
+  TriangleCountPlan plan;
+
+  TriangleCountPlan::Relabeling relabeling_flag =
+      relabel ? TriangleCountPlan::kRelabel : TriangleCountPlan::kAutoRelabel;
+
+  switch (algo) {
+  case TriangleCountPlan::kNodeIteration:
+    plan = TriangleCountPlan::NodeIteration(relabeling_flag);
+    break;
+
+  case TriangleCountPlan::kEdgeIteration:
+    plan = TriangleCountPlan::EdgeIteration(relabeling_flag);
+    break;
+
+  case TriangleCountPlan::kOrderedCount:
+    plan = TriangleCountPlan::OrderedCount(relabeling_flag);
+    break;
+
+  default:
+    std::cerr << "Unknown algo: " << algo << "\n";
+  }
+
+  auto num_triangles_result = TriangleCount(pfg.get(), plan);
+  if (!num_triangles_result) {
+    KATANA_LOG_FATAL(
+        "failed to run algorithm: {}", num_triangles_result.error());
+  }
+  auto num_triangles = num_triangles_result.value();
+
+  std::cout << "NumTriangles: " << num_triangles << "\n";
+
+  totalTime.stop();
+
+  return 0;
+}
---
+++ python/katana/analytics/_triangle_count.pyx
@@ -0,0 +1,124 @@
+from libc.stdint cimport uint64_t
+from libcpp cimport bool
+
+from katana.cpp.libstd.boost cimport handle_result_void, handle_result_assert, raise_error_code, std_result
+from katana.cpp.libgalois.graphs.Graph cimport PropertyFileGraph
+from katana.analytics.plan cimport Plan, _Plan
+from katana.property_graph cimport PropertyGraph
+
+from enum import Enum
+
+
+cdef extern from "katana/analytics/triangle_count/triangle_count.h" namespace "katana::analytics" nogil:
+    cppclass _TriangleCountPlan "katana::analytics::TriangleCountPlan" (_Plan):
+        enum Algorithm:
+            kNodeIteration "katana::analytics::TriangleCountPlan::kNodeIteration"
+            kEdgeIteration "katana::analytics::TriangleCountPlan::kEdgeIteration"
+            kOrderedCount "katana::analytics::TriangleCountPlan::kOrderedCount"
+
+        enum Relabeling:
+            kRelabel "katana::analytics::TriangleCountPlan::kRelabel"
+            kNoRelabel "katana::analytics::TriangleCountPlan::kNoRelabel"
+            kAutoRelabel "katana::analytics::TriangleCountPlan::kAutoRelabel"
+
+        _TriangleCountPlan.Algorithm algorithm() const
+        _TriangleCountPlan.Relabeling relabeling() const
+        bool edges_sorted() const
+
+        TriangleCountPlan()
+
+        @staticmethod
+        _TriangleCountPlan NodeIteration(bool edges_sorted, _TriangleCountPlan.Relabeling relabeling)
+        @staticmethod
+        _TriangleCountPlan EdgeIteration(bool edges_sorted, _TriangleCountPlan.Relabeling relabeling)
+        @staticmethod
+        _TriangleCountPlan OrderedCount(bool edges_sorted, _TriangleCountPlan.Relabeling relabeling)
+
+
+    _TriangleCountPlan.Relabeling kDefaultRelabeling "katana::analytics::TriangleCountPlan::kDefaultRelabeling"
+    bool kDefaultEdgeSorted "katana::analytics::TriangleCountPlan::kDefaultEdgeSorted"
+
+    std_result[uint64_t] TriangleCount(PropertyFileGraph* pfg, _TriangleCountPlan plan)
+
+
+class _TriangleCountPlanAlgorithm(Enum):
+    NodeIteration = _TriangleCountPlan.Algorithm.kNodeIteration
+    EdgeIteration = _TriangleCountPlan.Algorithm.kEdgeIteration
+    OrderedCount = _TriangleCountPlan.Algorithm.kOrderedCount
+
+
+cdef _relabeling_to_python(v):
+    if v == _TriangleCountPlan.Relabeling.kRelabel:
+        return True
+    elif v == _TriangleCountPlan.Relabeling.kNoRelabel:
+        return False
+    else:
+        return None
+
+
+cdef _relabeling_from_python(v):
+    if v is None:
+        return _TriangleCountPlan.Relabeling.kAutoRelabel
+    elif v:
+        return _TriangleCountPlan.Relabeling.kRelabel
+    else:
+        return _TriangleCountPlan.Relabeling.kNoRelabel
+
+
+cdef class TriangleCountPlan(Plan):
+    cdef:
+        _TriangleCountPlan underlying_
+
+    cdef _Plan* underlying(self) except NULL:
+        return &self.underlying_
+
+    Algorithm = _TriangleCountPlanAlgorithm
+
+    @staticmethod
+    cdef TriangleCountPlan make(_TriangleCountPlan u):
+        f = <TriangleCountPlan>TriangleCountPlan.__new__(TriangleCountPlan)
+        f.underlying_ = u
+        return f
+
+    @property
+    def algorithm(self) -> _TriangleCountPlanAlgorithm:
+        return _TriangleCountPlanAlgorithm(self.underlying_.algorithm())
+
+    @property
+    def edges_sorted(self) -> bool:
+        return self.underlying_.edges_sorted()
+
+    @property
+    def relabeling(self):
+        return _relabeling_to_python(self.underlying_.relabeling())
+
+    @staticmethod
+    def node_iteration(bool edges_sorted = kDefaultEdgeSorted,
+                       relabeling = _relabeling_to_python(kDefaultRelabeling)):
+        return TriangleCountPlan.make(_TriangleCountPlan.NodeIteration(
+            edges_sorted, _relabeling_from_python(relabeling)))
+
+    @staticmethod
+    def edge_iteration(bool edges_sorted = kDefaultEdgeSorted,
+                       relabeling = _relabeling_to_python(kDefaultRelabeling)):
+        return TriangleCountPlan.make(_TriangleCountPlan.EdgeIteration(
+            edges_sorted, _relabeling_from_python(relabeling)))
+
+    @staticmethod
+    def ordered_count(bool edges_sorted = kDefaultEdgeSorted,
+                      relabeling = _relabeling_to_python(kDefaultRelabeling)):
+        return TriangleCountPlan.make(_TriangleCountPlan.OrderedCount(
+            edges_sorted, _relabeling_from_python(relabeling)))
+
+
+cdef uint64_t handle_result_int(std_result[uint64_t] res) nogil except *:
+    if not res.has_value():
+        with gil:
+            raise_error_code(res.error())
+    return res.value()
+
+
+def triangle_count(PropertyGraph pg,  TriangleCountPlan plan = TriangleCountPlan()) -> int:
+    with nogil:
+        v = handle_result_int(TriangleCount(pg.underlying.get(), plan.underlying_))
+    return v

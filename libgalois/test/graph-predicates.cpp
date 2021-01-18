712c7f47c4f0ee049d53d8354ccc97d64533a86d
Arthur Peters <amp@katanagraph.com>
2021-01-18 00:31:08 -0600

    Fix bug in SourcePicker. Add regression test.
    This fixes the same bug as
    https://github.com/IntelligentSoftwareSystems/Galois/commit/088fe7576402424cbc9964b99c5bc09222421b3e

---
+++ libgalois/test/graph-predicates.cpp
@@ -0,0 +1,42 @@
+#include <llvm/Support/CommandLine.h>
+
+#include "TestPropertyGraph.h"
+#include "katana/Logging.h"
+#include "katana/PropertyFileGraph.h"
+#include "katana/SharedMemSys.h"
+#include "katana/analytics/Utils.h"
+
+namespace cll = llvm::cl;
+
+static cll::opt<std::string> rmat10InputFile(
+    cll::Positional, cll::desc("<rmat10 input file>"), cll::Required);
+
+void
+TestIsApproximateDegreeDistributionPowerLaw() {
+  {
+    LinePolicy policy{11};
+    auto g = MakeFileGraph<uint32_t>(100, 1, &policy);
+
+    KATANA_LOG_ASSERT(g->size() == 100);
+    KATANA_LOG_ASSERT(g->num_edges() == 11 * 100);
+
+    KATANA_LOG_ASSERT(
+        !katana::analytics::IsApproximateDegreeDistributionPowerLaw(*g.get()));
+  }
+  {
+    auto g = katana::PropertyFileGraph::Make(rmat10InputFile);
+    KATANA_LOG_ASSERT(
+        katana::analytics::IsApproximateDegreeDistributionPowerLaw(
+            *g.assume_value().get()));
+  }
+}
+
+int
+main(int argc, char** argv) {
+  katana::SharedMemSys sys;
+  cll::ParseCommandLineOptions(argc, argv);
+
+  TestIsApproximateDegreeDistributionPowerLaw();
+
+  return 0;
+}

a36859a84a62e301dc1a0a3747321f43c98f5796
Gurbinder Gill <gill@cs.utexas.edu>
2021-02-05 17:25:51 -0600

    Fix gr to kg graph conversion

--- libtsuba/include/tsuba/CSRTopology.h
+++ libtsuba/include/tsuba/CSRTopology.h
@@ -30,7 +30,7 @@ constexpr uint64_t
 CSRTopologyFileSize(const CSRTopologyHeader& header) {
   uint64_t edge_size =
       header.version == 1 ? sizeof(uint32_t) : sizeof(uint64_t);
-  return sizeof(header) + ((header.num_nodes - 1) * sizeof(uint64_t)) +
+  return sizeof(header) + ((header.num_nodes) * sizeof(uint64_t)) +
          katana::AlignUp<uint64_t>(header.num_edges * edge_size) +
          (header.num_edges * header.edge_type_size);
 }

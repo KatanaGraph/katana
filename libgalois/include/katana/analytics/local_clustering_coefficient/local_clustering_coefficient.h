5b0446b9711a5b3cdb4bc31cc7e63376384ead90
Gurbinder Gill <gill@cs.utexas.edu>
2021-03-14 17:16:07 -0500

    Add local clustering coefficient to Katana library.
    Co-authored-by: Udit Agarwal <uditagar@users.noreply.github.com>

---
+++ libgalois/include/katana/analytics/local_clustering_coefficient/local_clustering_coefficient.h
@@ -0,0 +1,89 @@
+#ifndef KATANA_LIBGALOIS_KATANA_ANALYTICS_LOCALCLUSTERINGCOEFFICIENT_LOCALCLUSTERINGCOEFFICIENT_H_
+#define KATANA_LIBGALOIS_KATANA_ANALYTICS_LOCALCLUSTERINGCOEFFICIENT_LOCALCLUSTERINGCOEFFICIENT_H_
+
+#include "katana/PropertyGraph.h"
+#include "katana/TypedPropertyGraph.h"
+#include "katana/analytics/Plan.h"
+#include "katana/analytics/Utils.h"
+
+// API
+
+namespace katana::analytics {
+
+/// A computational plan to for computing Local
+/// Clustering Coefficient of the nodes in the graph.
+class LocalClusteringCoefficientPlan : public Plan {
+public:
+  enum Algorithm { kOrderedCountAtomics, kOrderedCountPerThread };
+
+  enum Relabeling {
+    kRelabel,
+    kNoRelabel,
+    kAutoRelabel,
+  };
+
+  static const Relabeling kDefaultRelabeling = kAutoRelabel;
+  static const bool kDefaultEdgeSorted = false;
+
+private:
+  Algorithm algorithm_;
+  Relabeling relabeling_;
+  bool edges_sorted_;
+
+  LocalClusteringCoefficientPlan(
+      Architecture architecture, Algorithm algorithm, bool edges_sorted,
+      Relabeling relabeling)
+      : Plan(architecture),
+        algorithm_(algorithm),
+        relabeling_(relabeling),
+        edges_sorted_(edges_sorted) {}
+
+public:
+  LocalClusteringCoefficientPlan()
+      : LocalClusteringCoefficientPlan{
+            kCPU, kOrderedCountPerThread, kDefaultEdgeSorted,
+            kDefaultRelabeling} {}
+
+  Algorithm algorithm() const { return algorithm_; }
+  Relabeling relabeling() const { return relabeling_; }
+  bool edges_sorted() const { return edges_sorted_; }
+
+  /**
+   * An ordered count algorithm that sorts the nodes by degree before
+   * execution. This has been found to give good performance. We implement the
+   * ordered count algorithm from the following:
+   * http://gap.cs.berkeley.edu/benchmark.html
+   *
+   * @param edges_sorted Are the edges of the graph already sorted.
+   * @param relabeling Should the algorithm relabel the nodes.
+   */
+  static LocalClusteringCoefficientPlan LocalClusteringCoefficientAtomics(
+      bool edges_sorted = kDefaultEdgeSorted,
+      Relabeling relabeling = kDefaultRelabeling) {
+    return {kCPU, kOrderedCountAtomics, edges_sorted, relabeling};
+  }
+
+  static LocalClusteringCoefficientPlan LocalClusteringCoefficientPerThread(
+      bool edges_sorted = kDefaultEdgeSorted,
+      Relabeling relabeling = kDefaultRelabeling) {
+    return {kCPU, kOrderedCountPerThread, edges_sorted, relabeling};
+  }
+};
+
+/**
+ * Count the total number of triangles in the graph. The graph must be
+ * symmetric!
+ *
+ * This algorithm copies the graph internally.
+ *
+ * @param pg The graph to process.
+ * @param output_property_name name of the output property
+ * @param plan
+ */
+KATANA_EXPORT Result<void> LocalClusteringCoefficient(
+    PropertyGraph* pg, const std::string& output_property_name,
+    LocalClusteringCoefficientPlan plan = {});
+
+}  // namespace katana::analytics
+
+#endif
---
+++ lonestar/analytics/cpu/local_clustering_coefficient/local_clustering_coefficient_cli.cpp
@@ -0,0 +1,97 @@
+/*
+ * This file belongs to the Galois project, a C++ library for exploiting
+ * parallelism. The code is being released under the terms of the 3-Clause BSD
+ * License (a copy is located in LICENSE.txt at the top-level directory).
+ *
+ * Copyright (C) 2018, The University of Texas at Austin. All rights reserved.
+ * UNIVERSITY EXPRESSLY DISCLAIMS ANY AND ALL WARRANTIES CONCERNING THIS
+ * SOFTWARE AND DOCUMENTATION, INCLUDING ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR ANY PARTICULAR PURPOSE, NON-INFRINGEMENT AND WARRANTIES OF
+ * PERFORMANCE, AND ANY WARRANTY THAT MIGHT OTHERWISE ARISE FROM COURSE OF
+ * DEALING OR USAGE OF TRADE.  NO WARRANTY IS EITHER EXPRESS OR IMPLIED WITH
+ * RESPECT TO THE USE OF THE SOFTWARE OR DOCUMENTATION. Under no circumstances
+ * shall University be liable for incidental, special, indirect, direct or
+ * consequential damages or loss of profits, interruption of business, or
+ * related expenses which may arise from use of Software or Documentation,
+ * including but not limited to those resulting from defects in Software and/or
+ * Documentation, or loss or inaccuracy of data of any kind.
+ */
+
+#include <iostream>
+
+#include "Lonestar/BoilerPlate.h"
+#include "katana/analytics/local_clustering_coefficient/local_clustering_coefficient.h"
+
+using namespace katana::analytics;
+
+const char* name = "Local Clustering Coefficient";
+const char* desc = "Computes the local clustering coefficient for each node";
+
+namespace cll = llvm::cl;
+
+static cll::opt<std::string> inputFile(
+    cll::Positional, cll::desc("<input file>"), cll::Required);
+static cll::opt<LocalClusteringCoefficientPlan::Algorithm> algo(
+    "algo", cll::desc("Choose an algorithm:"),
+    cll::values(clEnumValN(
+        LocalClusteringCoefficientPlan::kOrderedCountAtomics,
+        "orderedCountAtomics", "Ordered Simple Count using Atomics")),
+    cll::values(clEnumValN(
+        LocalClusteringCoefficientPlan::kOrderedCountPerThread,
+        "orderedCountPerThread",
+        "Ordered Simple Count using PerThreadStorage (default)")),
+    cll::init(LocalClusteringCoefficientPlan::kOrderedCountPerThread));
+
+static cll::opt<bool> relabel(
+    "relabel",
+    cll::desc("Relabel nodes of the graph (default value of false => "
+              "choose automatically)"),
+    cll::init(false));
+int
+main(int argc, char** argv) {
+  std::unique_ptr<katana::SharedMemSys> G =
+      LonestarStart(argc, argv, name, desc, nullptr, &inputFile);
+
+  katana::StatTimer totalTime("TimerTotal");
+  totalTime.start();
+
+  if (!symmetricGraph) {
+    KATANA_DIE(
+        "This application requires a symmetric graph input;"
+        " please use the -symmetricGraph flag "
+        " to indicate the input is a symmetric graph.");
+  }
+
+  std::cout << "Reading from file: " << inputFile << "\n";
+  std::unique_ptr<katana::PropertyGraph> pg =
+      MakeFileGraph(inputFile, edge_property_name);
+
+  LocalClusteringCoefficientPlan plan;
+
+  LocalClusteringCoefficientPlan::Relabeling relabeling_flag =
+      relabel ? LocalClusteringCoefficientPlan::kRelabel
+              : LocalClusteringCoefficientPlan::kAutoRelabel;
+
+  switch (algo) {
+  case LocalClusteringCoefficientPlan::kOrderedCountAtomics:
+    plan = LocalClusteringCoefficientPlan::LocalClusteringCoefficientAtomics(
+        relabeling_flag);
+    break;
+  case LocalClusteringCoefficientPlan::kOrderedCountPerThread:
+    plan = LocalClusteringCoefficientPlan::LocalClusteringCoefficientPerThread(
+        relabeling_flag);
+    break;
+  default:
+    std::cerr << "Unknown algo: " << algo << "\n";
+  }
+
+  auto lcc_result =
+      LocalClusteringCoefficient(pg.get(), "localClusteringCoefficient", plan);
+  if (!lcc_result) {
+    KATANA_LOG_FATAL("Failed to run algorithm: {}", lcc_result.error());
+  }
+
+  totalTime.stop();
+
+  return 0;
+}

aec3961d25d6f06a461ba2136bfaf6a66aacc56b
Arthur Peters <amp@katanagraph.com>
2021-03-26 16:06:42 -0500

    Switch CI to use Mambaforge instead of miniconda.
    This reduces CI environment setup time noticably.
    
    https://github.com/conda-forge/miniforge#mambaforge

--- .github/workflows/activate_miniconda.sh
+++ .github/workflows/activate_miniconda.sh
@@ -1,7 +1,7 @@
 # This is a bash INCLUDE file. Source it don't run it.
 
 if [ '!' -d $HOME/miniconda/bin ]; then
-  bash "$HOME/.cache/miniconda/miniconda.sh" -f -b -p $HOME/miniconda
+  bash "$HOME/.cache/miniconda/mambaforge.sh" -f -b -p $HOME/miniconda
 fi
 export PATH=$HOME/miniconda/bin:$PATH
 source activate

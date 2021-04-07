8cb11a67cef21cd0578e8381f0e4ffd4e2ba4c62
mohan <mohanksriram@gmail.com>
2021-04-07 13:23:27 +0530

    support: Expose katana library version in the Python API

--- python/katana/galois.pyx
+++ python/katana/galois.pyx
@@ -1,7 +1,12 @@
 from .cpp.libgalois.Galois cimport setActiveThreads as c_setActiveThreads
+from .cpp.libgalois.Galois cimport getVersion as c_getVersion
+
 
 _katana_runtime = _katana_runtime_wrapper()
 
 def setActiveThreads(int n):
     return c_setActiveThreads(n)
+
+def get_version():
+    return c_getVersion()
 

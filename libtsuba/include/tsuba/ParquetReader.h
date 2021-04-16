44ef570752c1496d1a44ea7105444c301351dc55
Tyler Hunt <thunt@katanagraph.com>
2021-04-16 16:42:42 +0000

    tsuba: add an interface to read number of rows
    Call should be cheap because it just looks at the metadata.
    
    Signed-off-by: Tyler Hunt <thunt@katanagraph.com>

--- libtsuba/include/tsuba/ParquetReader.h
+++ libtsuba/include/tsuba/ParquetReader.h
@@ -68,6 +68,10 @@ public:
   ///   \param uri an identifier for a parquet file
   katana::Result<int32_t> NumColumns(const katana::Uri& uri);
 
+  /// Get the number of rows for the table stored in a parquet file
+  ///   \param uri an identifier for a parquet file
+  katana::Result<int64_t> NumRows(const katana::Uri& uri);
+
 private:
   ParquetReader(std::optional<Slice> slice, bool make_cannonical)
       : slice_(slice), make_cannonical_{make_cannonical} {}

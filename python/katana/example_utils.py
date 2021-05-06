d5c5baf4e818912e486d554f0a2bc2f22d071879
Arthur Peters <amp@katanagraph.com>
2021-05-06 17:31:16 -0500

    Update inputs to v19 and improve python inputs download.

--- python/katana/example_utils.py
+++ python/katana/example_utils.py
@@ -1,26 +1,34 @@
 import os
+from pathlib import Path
 import tarfile
 import urllib.request
+import shutil
 
 __all__ = ["get_input"]
 
 
-def get_cache_directory():
-    # Use the CI paths if they exist, just to make CI integration easier.
-    ci_inputs_path = os.environ["HOME"] + "/.cache/graph"
-    if os.path.isdir(ci_inputs_path):
-        return ci_inputs_path
-    # Otherwise use a more specific path name
-    return os.environ["HOME"] + "/.cache/galois"
-
-
-def get_inputs_directory():
-    cache_dir = get_cache_directory()
-    inputs_dir = cache_dir + "/inputs"
-    if os.path.isdir(inputs_dir) and os.path.isdir(inputs_dir + "/propertygraphs/ldbc_003"):
+def get_inputs_directory(*, invalidate=False):
+    inputs_dir = None
+    # Use the build paths if they exist.
+    paths_to_check = list(Path(__file__).parents) + list(Path.cwd().parents)
+    for path in paths_to_check:
+        # TODO(amp): If we can abstract the input version info a shared file, this should look for
+        #  specifically that version.
+        ci_inputs_path = path / "inputs" / "current"
+        if ci_inputs_path.is_dir():
+            inputs_dir = ci_inputs_path
+    # Otherwise use a cache directory
+    if not inputs_dir:
+        inputs_dir = Path(os.environ["HOME"]) / ".cache" / "katana" / "inputs"
+    if not invalidate and inputs_dir.is_dir() and (inputs_dir / "propertygraphs" / "ldbc_003").is_dir():
         return inputs_dir
+    try:
+        shutil.rmtree(inputs_dir)
+    except OSError:
+        inputs_dir.unlink()
+    inputs_dir.mkdir(parents=True, exist_ok=True)
     fn, _headers = urllib.request.urlretrieve(
-        "https://katana-ci-public.s3.us-east-1.amazonaws.com/inputs/katana-inputs-v18.tar.gz"
+        "https://katana-ci-public.s3.us-east-1.amazonaws.com/inputs/katana-inputs-v19.tar.gz"
     )
     try:
         with tarfile.open(fn) as tar:
@@ -30,11 +38,15 @@ def get_inputs_directory():
     return inputs_dir
 
 
-def get_input(path):
+def get_input(rel_path):
     """
     Download the standard Galois inputs (with local caching on disk) and return a path to a file in that archive.
 
     >>> from katana.property_graph import PropertyGraph
     ... graph = PropertyGraph(get_input("propertygraphs/ldbc_003"))
     """
-    return get_inputs_directory() + "/" + path
+    path = get_inputs_directory() / rel_path
+    if path.exists():
+        return path
+    else:
+        return get_inputs_directory(invalidate=True) / rel_path

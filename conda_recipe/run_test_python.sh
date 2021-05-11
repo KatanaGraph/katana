d06dbee9125a5d2e7615f77ab0d074c31a0c9914
Arthur Peters <amp@katanagraph.com>
2021-05-10 23:09:23 -0500

    Add Katana Tutorial to testing.

--- conda_recipe/run_test_python.sh
+++ conda_recipe/run_test_python.sh
@@ -2,7 +2,8 @@
 set -e
 
 echo "Testing Jupyter..."
-jupyter nbconvert --execute --to markdown python/examples/jupyter/jaccard_numba.ipynb
+jupyter nbconvert --execute --to markdown "python/examples/jupyter/jaccard_numba.ipynb"
+jupyter nbconvert --execute --to markdown "python/examples/jupyter/Katana Tutorial.ipynb"
 
 echo "Testing with pytest..."
 pytest -v python/test

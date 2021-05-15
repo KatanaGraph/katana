8967ac5ab24a622c93ba1bf210130fa9903734a6
Donald Nguyen <ddn0@users.noreply.github.com>
2021-05-14 21:14:13 -0700

    build: Parameterize Python support scripts

--- test/install_conda_packages_test.py
+++ test/install_conda_packages_test.py
@@ -14,11 +14,26 @@ def main():
     parser.add_argument("package_dir", type=Path, help="The directory containing the conda packages to install.")
     parser.add_argument("docker_image", type=str, help="The docker image to use.")
 
+    parser.add_argument("--python-package", default="katana-python", help="Name of python conda package to test")
+    parser.add_argument("--tools-package", default="katana-tools", help="Name of tools conda package to test")
+    parser.add_argument(
+        "--tools-test-command", default="graph-convert --version", help="Command to use to test tools package"
+    )
+
     args = parser.parse_args()
 
-    with Popen(
-        ["docker", "build", "-", "--build-arg", f"BASE_IMAGE={args.docker_image}",], stdin=PIPE
-    ) as docker_proc, tarfile.open(fileobj=docker_proc.stdin, mode="w:gz") as context_tar:
+    build_args = [
+        f"BASE_IMAGE={args.docker_image}",
+        f"PYTHON_PACKAGE={args.python_package}",
+        f"TOOLS_PACKAGE={args.tools_package}",
+        f"TOOLS_TEST_COMMAND={args.tools_test_command}",
+    ]
+    command = ["docker", "build", "-"]
+    for b in build_args:
+        command.append("--build-arg")
+        command.append(b)
+
+    with Popen(command, stdin=PIPE) as docker_proc, tarfile.open(fileobj=docker_proc.stdin, mode="w:gz") as context_tar:
         context_tar.add(Path(args.package_dir).absolute(), arcname="packages")
         context_tar.add(Path(__file__).parent / "install_conda_packages_test.Dockerfile", arcname="Dockerfile")
         context_tar.add(

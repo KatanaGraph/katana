79f62e99bb786f51b3d3f9a90553f8d57725ac18
Zhipeng Jia <zhipeng.jia@outlook.com>
2021-05-27 22:05:22 -0500

    build: Fix for building with Ninja

--- cmake/Modules/KatanaPythonSetupSubdirectory.cmake
+++ cmake/Modules/KatanaPythonSetupSubdirectory.cmake
@@ -219,7 +219,7 @@ function(add_python_setuptools_target TARGET_NAME)
       ALL
       COMMAND ${PYTHON_SETUP_COMMAND} ${quiet} build "$<$<CONFIG:Debug>:--debug>" ${parallel}
       COMMAND install ${PYTHON_ENV_SCRIPT}.tmp ${PYTHON_ENV_SCRIPT}
-      BYPRODUCTS ${PYTHON_BINARY_DIR} ${PYTHON_ENV_SCRIPT} ${CMAKE_BINARY_DIR}/katana_setup_requirements_cache.txt
+      BYPRODUCTS ${PYTHON_BINARY_DIR} ${PYTHON_ENV_SCRIPT}
       WORKING_DIRECTORY ${PYTHON_BINARY_DIR}
       COMMENT "Building ${TARGET_NAME} in symlink tree ${PYTHON_BINARY_DIR}"
   )
@@ -231,6 +231,14 @@ function(add_python_setuptools_target TARGET_NAME)
       COMMENT "bdist ${TARGET_NAME} in symlink tree ${PYTHON_BINARY_DIR}"
   )
   add_dependencies(${TARGET_NAME}_wheel ${TARGET_NAME})
+
+  # When building with Ninja, Ninja refuses to delete non-empty directories when cleaning.
+  # Thus ${PYTHON_BINARY_DIR} is included here, which explicitly removes the directory for the clean target.
+  set_property(
+      TARGET ${TARGET_NAME}
+      APPEND
+      PROPERTY ADDITIONAL_CLEAN_FILES ${PYTHON_BINARY_DIR} ${CMAKE_BINARY_DIR}/katana_setup_requirements_cache.txt
+  )
 
   add_dependencies(${TARGET_NAME} ${TARGET_NAME}_python_tree ${TARGET_NAME}_setup_tree ${TARGET_NAME}_scripts_tree)
   if(X_DEPENDS)

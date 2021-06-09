f87b92086c9c45bbdf3167da026b6163fc21dbbe
Patrick Kenney <patrickkenney9801@gmail.com>
2021-06-09 14:22:55 -0500

    utility: add approximation function for an arrow table's memory usage (#276)

--- libsupport/include/katana/ArrowInterchange.h
+++ libsupport/include/katana/ArrowInterchange.h
@@ -310,6 +310,12 @@ KATANA_EXPORT void DiffFormatTo(
 KATANA_EXPORT uint64_t
 ApproxArrayMemUse(const std::shared_ptr<arrow::Array>& array);
 
+/// Estimate the amount of memory this table is using
+/// n.b. Estimate is best effort when an array is a slice or a variable type like
+///   large_string; it will be an upper bound in those cases
+KATANA_EXPORT uint64_t
+ApproxTableMemUse(const std::shared_ptr<arrow::Table>& table);
+
 }  // namespace katana
 
 #endif

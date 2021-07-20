f2beb5521c19e22d0a1999be35b0fabb465c67f8
Serge Rogatch <serge.rogatch@gmail.com>
2021-07-20 23:37:35 +0200

    Remove unneeded code from open repo

--- libsupport/include/katana/HostAllocator.h
+++ libsupport/include/katana/HostAllocator.h
@@ -7,17 +7,7 @@
 
 namespace katana {
 
-//TODO (serge): change to a polymorphic allocator to switch between pinned and swappable memory
 enum class MemoryPinType { Swappable = 0, Pinned = 1 };
-
-#ifdef KATANA_ENABLE_GPU
-static constexpr const bool kGpuEnabled = true;
-static constexpr const MemoryPinType kUseMemoryPinType = MemoryPinType::Pinned;
-#else
-static constexpr const bool kGpuEnabled = false;
-static constexpr const MemoryPinType kUseMemoryPinType =
-    MemoryPinType::Swappable;
-#endif  // KATANA_ENABLE_GPU
 
 class BaseHostAllocator {
 public:

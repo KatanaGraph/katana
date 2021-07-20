48bb267f081b87ad9813b7556ee185bfa135ccad
Serge Rogatch <serge.rogatch@gmail.com>
2021-07-20 22:02:34 +0200

    Remove kGpuEnabled constant

--- libsupport/include/katana/CpuGpuSwitch.h
+++ libsupport/include/katana/CpuGpuSwitch.h
@@ -6,10 +6,8 @@ namespace katana {
 enum class MemoryPinType { Swappable = 0, Pinned = 1 };
 
 #ifdef KATANA_ENABLE_GPU
-static constexpr const bool kGpuEnabled = true;
 static constexpr const MemoryPinType kUseMemoryPinType = MemoryPinType::Pinned;
 #else
-static constexpr const bool kGpuEnabled = false;
 static constexpr const MemoryPinType kUseMemoryPinType =
     MemoryPinType::Swappable;
 #endif  // KATANA_ENABLE_GPU

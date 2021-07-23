d831ef2cea64a53635c18eeef2e1f9c8f5d525bd
nirjhar <nirjhar@wakizashi.katanagraph.com>
2021-07-23 23:28:27 +0000

    Does overread, need future optimization

--- libtsuba/src/ParquetReader.cpp
+++ libtsuba/src/ParquetReader.cpp
@@ -283,11 +283,7 @@ public:
   Result<std::shared_ptr<arrow::Table>> ReadTable(
       std::vector<int32_t> col_indexes,
       std::optional<tsuba::ParquetReader::Slice> slice = std::nullopt) {
-    if (slice) {
-      return KATANA_ERROR(
-          katana::ErrorCode::NotImplemented,
-          "column subset read not implemented for slice! Sorry!");
-    }
+    
 
     std::vector<std::shared_ptr<arrow::Table>> tables;
 
@@ -322,7 +318,12 @@ public:
       }
       tables.emplace_back(arrow::Table::Make(arrow::schema(fields), columns));
     }
-    return KATANA_CHECKED(arrow::ConcatenateTables(tables));
+
+    auto concatenated_tables = arrow::ConcatenateTables(tables);
+    if (slice) {
+	concatenated_tables.ValueOrDie() = concatenated_tables.ValueOrDie()->Slice(slice->offset, slice->length);
+    }
+    return KATANA_CHECKED(concatenated_tables);
   }
 
 private:

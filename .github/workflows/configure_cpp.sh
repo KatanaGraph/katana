64b1191f4ae8ea0a6e50ea7aad7a396f8cb54be5
Serge Rogatch <serge.rogatch@gmail.com>
2021-07-27 20:27:53 +0200

    Fix GPU build

--- .github/workflows/configure_cpp.sh
+++ .github/workflows/configure_cpp.sh
@@ -12,13 +12,13 @@ case $OS in
   *macOS*) export PATH=$PATH:/usr/local/opt/llvm/bin ;;
   *-gpu)
     # TODO (serge): Reenable -DKATANA_COMPONENTS=ava once its build issue is resolved https://katanagraph.atlassian.net/browse/KAT-1035
-    GPU_ARGS = "-DCMAKE_CUDA_COMPILER_LAUNCHER=ccache"
-    GPU_ARGS = "$GPU_ARGS -DCMAKE_CUDA_ARCHITECTURES=\"75\""
-    GPU_ARGS = "$GPU_ARGS -DCMAKE_CUDA_COMPILER=\"/usr/local/cuda/bin/nvcc\""
-    GPU_ARGS = "$GPU_ARGS -DKATANA_NUM_TEST_GPUS=2"
+    GPU_ARGS="-DCMAKE_CUDA_COMPILER_LAUNCHER=ccache"
+    GPU_ARGS="$GPU_ARGS -DCMAKE_CUDA_ARCHITECTURES=\"75\""
+    GPU_ARGS="$GPU_ARGS -DCMAKE_CUDA_COMPILER=\"/usr/local/cuda/bin/nvcc\""
+    GPU_ARGS="$GPU_ARGS -DKATANA_NUM_TEST_GPUS=2"
     if ["$CI_BUILD_TYPE" == "Sanitizer"]; then
-      GPU_ARGS = "$GPU_ARGS -DCMAKE_LINKER=$CXX"
-      GPU_ARGS = "$GPU_ARGS -DCMAKE_CUDA_LINK_EXECUTABLE=\"<CMAKE_LINKER> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>\""
+      GPU_ARGS="$GPU_ARGS -DCMAKE_LINKER=$CXX"
+      GPU_ARGS="$GPU_ARGS -DCMAKE_CUDA_LINK_EXECUTABLE=\"<CMAKE_LINKER> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>\""
     fi
     ;;
   *) ;;

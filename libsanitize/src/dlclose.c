5b53e79f2f19bf6723536cc114df2af4f9927a9f
Serge Rogatch <serge.rogatch@gmail.com>
2021-07-28 15:53:04 +0200

    Add fake dlclose() to retain shared library symbols in sanitized program

---
+++ libsanitize/src/dlclose.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+// This function prevents unloading of shared libraries, thus the sanitizers
+// can always find the address that belonged to a shared library.
+// See https://github.com/google/sanitizers/issues/89#issuecomment-484435084
+int dlclose(void*) { return 0; }

70bf87f5a4a2bb0894fce68966505d9d8227d532
Serge Rogatch <serge.rogatch@gmail.com>
2021-07-28 16:39:40 +0200

    Fix CI

--- libsanitize/src/dlclose.c
+++ libsanitize/src/dlclose.c
@@ -3,4 +3,4 @@
 // This function prevents unloading of shared libraries, thus the sanitizers
 // can always find the address that belonged to a shared library.
 // See https://github.com/google/sanitizers/issues/89#issuecomment-484435084
-int dlclose(void*) { return 0; }
+int dlclose(void* ptr) { return 0; }

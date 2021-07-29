6e0ee99aa568b85446efd4b590d81366d081bb51
iso-p <insomnia.px@gmail.com>
2021-07-29 09:57:56 -0600

    update python CI for metagraph plugin

--- metagraph-plugin/metagraph_katana/tests/test_types.py
+++ metagraph-plugin/metagraph_katana/tests/test_types.py
@@ -2,6 +2,9 @@ import metagraph as mg
 import pytest
 
 
+def test_ci_setup():
+    assert False
+
 def test_num_nodes(katanagraph_rmat15_cleaned_di):
     cnt = 0
     for nid in katanagraph_rmat15_cleaned_di.value:

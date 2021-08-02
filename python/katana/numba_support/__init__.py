2b3379e3d68164dc28b97da20e3ed53b95337ebe
Arthur Peters <amp@katanagraph.com>
2021-08-02 16:16:19 -0500

    document exec_in_file

--- python/katana/numba_support/__init__.py
+++ python/katana/numba_support/__init__.py
@@ -3,6 +3,7 @@ import tempfile
 from pathlib import Path
 from typing import Optional
 
+# A temporary directory in which to write python code to exec. See exec_in_file.
 TEMPORARY_DIRECTORY = None
 if os.environ.get("KATANA_WRITE_CODE"):
     # pylint: disable=consider-using-with
@@ -12,6 +13,13 @@ if os.environ.get("KATANA_WRITE_CODE"):
 
 
 def exec_in_file(filename: str, source: str, globals_dict: Optional[dict] = None, locals_dict: Optional[dict] = None):
+    """
+    Execute ``source`` just as `exec`. Also write the code to ``filename`` (in a temporary directory that is deleted at
+    interpreter shutdown.) and associate the execution with that file.
+
+    This improve debuggability because it allows the source to be found by the `inspect` module. This helps when
+    debugging numba compilation problems.
+    """
     source = source.strip()
     if not filename.endswith(".py"):
         filename += ".py"

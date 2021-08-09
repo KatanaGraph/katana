cbcc7888ac74a58b3dd0ec1a16f299b44a4cdaac
Arthur Peters <amp@katanagraph.com>
2021-08-09 17:28:52 -0500

    python: Implement more input formats and types.

--- python/katana/native_interfacing/buffer_access.pyx
+++ python/katana/native_interfacing/buffer_access.pyx
@@ -1,4 +1,5 @@
 import numpy
+import pyarrow
 
 from cpython.buffer cimport PyBUF_CONTIG, PyBuffer_Release, PyObject_CheckBuffer, PyObject_GetBuffer
 
@@ -24,3 +25,40 @@ cdef class UntypedBufferAccess:
 
     def empty_like(self, *args, **kwds):
         return numpy.empty(*args, dtype=self._numpy_dtype, **kwds)
+
+
+cpdef to_numpy(v):
+    """
+    Convert a value to numpy using it's own conversion methods if possible.
+
+    This tries four different ways to convert:
+
+    1. If ``v`` is a numpy array already, return it.
+    2. If ``v`` has a method ``to_numpy``, call it and return the result. (supports pyarrow, pandas, etc.)
+    3. If ``v`` has a method ``as_numpy``, call it and return the result. (supports tensorflow, etc.)
+    3. Call `numpy.array` and return the result.
+
+    In all cases, this function attempts to avoid a copy, but does not guarantee it. The result should be treated as
+    immutable in most cases.
+    """
+    if isinstance(v, numpy.ndarray):
+        return v
+    if hasattr(v, "to_numpy"):
+        return v.to_numpy()
+    if hasattr(v, "as_numpy"):
+        return v.as_numpy()
+    return numpy.array(v, copy=False)
+
+
+cpdef to_pyarrow(v):
+    """
+    Convert an array to pyarrow.
+
+    This tries two different ways to convert:
+
+    1. If ``v`` is a pyarrow array already, return it.
+    3. Call `pyarrow.array` and return the result.
+    """
+    if isinstance(v, pyarrow.Array):
+        return v
+    return pyarrow.array(v)

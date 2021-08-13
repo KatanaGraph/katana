7f8605b5d4db1c6ca00523d201dccafe2880b255
iso-p <insomnia.px@gmail.com>
2021-08-13 13:16:04 -0600

    update version

--- metagraph-plugin/setup.py
+++ metagraph-plugin/setup.py
@@ -1,11 +1,10 @@
 from setuptools import find_packages, setup
 
-#import katana
+import katana
 
 setup(
     name="metagraph-katana",
-    #version=katana.__version__,
-    version='0.0.1',
+    version=katana.__version__,
     # currently the version is the same as katana.__version__ (which actually comes from the version built into libsupport).
     # Eventually we need to have separate versions built into each library
     description="katana plugins for Metagraph",

d4a668159617efc546a3ea15874fe546dcc8886d
iso-p <insomnia.px@gmail.com>
2021-08-19 11:22:44 -0600

    update pytest flags

--- metagraph-plugin/tests/conftest.py
+++ metagraph-plugin/tests/conftest.py
@@ -81,5 +81,3 @@ def pytest_addoption(parser):
 def pytest_runtest_setup(item):
     if 'runslow' in item.keywords and not item.config.getoption("--runslow"):
         pytest.skip("need --runslow option to run this test")
-
-

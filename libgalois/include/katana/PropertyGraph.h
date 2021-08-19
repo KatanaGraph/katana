19912ceed87206477d963cfdf735d665cbf877d1
nirjhar <nirjhar@wakizashi.katanagraph.com>
2021-08-19 15:48:53 +0000

    Minor

--- libgalois/include/katana/PropertyGraph.h
+++ libgalois/include/katana/PropertyGraph.h
@@ -152,14 +152,20 @@ private:
     node_property_indexes_column_name =
         rdg_.get_node_property_indexes_column_name();
     for (const std::string column_name : node_property_indexes_column_name) {
-      MakeNodeIndex(column_name);
+      auto result = MakeNodeIndex(column_name);
+      if (!result) {
+        return (void)result.error();
+      }
     }
   }
   void recreate_edge_property_indexes() {
     edge_property_indexes_column_name =
         rdg_.get_edge_property_indexes_column_name();
     for (const std::string column_name : edge_property_indexes_column_name) {
-      MakeEdgeIndex(column_name);
+      auto result = MakeEdgeIndex(column_name);
+      if (!result) {
+        return (void)result.error();
+      }
     }
   }
 

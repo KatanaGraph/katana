db48c80c41cd6b8e78fc7adf73618f5e944205a3
witchel <witchel@cs.utexas.edu>
2021-08-23 20:22:05 -0500

    libsupport Cache provides an LRU cache (#466)
    Move concurrent map and dependents to libsupport
    
    Use the same code as libgluon for phlib.
    
    This is best for now since switching to find_c_library actually
    breaks the other code and that would force coordinated commits
    and make Emmett very sad.

---
+++ libsupport/include/katana/Cache.h
@@ -0,0 +1,166 @@
+#ifndef KATANA_LIBSUPPORT_KATANA_CACHE_H_
+#define KATANA_LIBSUPPORT_KATANA_CACHE_H_
+
+// This is single threaded only, it is not intended to store large objects,
+// but rather metadata (e.g., a shared_ptr to a property column).
+
+// The problem witchel had implementing a multi-threaded version using
+// parallel-hashmap is a lock ordering problem.  parallel-hashmap 1.33 allows
+// execution of code with the write lock held, only when modifying, not when adding an
+// element.  We have to modify the LRU list when eviciting an element, so the natural
+// lock ordering is parallel-hashmap write lock, then list lock.  But without a way to
+// execute insert code with the parallel-hashmap write lock held, it seemed like there
+// would be some form of race condition.
+
+#include <list>
+#include <optional>
+#include <string>
+
+#include <boost/container_hash/hash.hpp>
+
+#include "katana/Logging.h"
+
+namespace tsuba {
+
+template <typename Key, typename Value>
+class KATANA_EXPORT Cache {
+  using ListType = std::list<Key>;
+  struct MapValue {
+    Value value;
+    // This allows us to delete the old position in the LRU list without a scan
+    typename ListType::iterator lru_it;
+  };
+  using MapType = std::unordered_map<Key, MapValue>;
+  enum class ReplacementPolicy { kLRUSize, kLRUBytes };
+
+public:
+  /// Construct an LRU cache that has a fixed number of entries.
+  Cache(
+      size_t capacity,  // number of entries
+      std::function<void(const Key& key)> evict_cb = nullptr)
+      : policy_(ReplacementPolicy::kLRUSize),
+        capacity_(capacity),
+        value_to_bytes_(nullptr),
+        evict_cb_(std::move(evict_cb)) {
+    KATANA_LOG_VASSERT(capacity_ > 0, "cache requires positive capacity");
+  }
+  /// Construct an LRU cache that holds fixed number of bytes.
+  Cache(
+      size_t capacity,  // bytes of entries
+      std::function<size_t(const Value& value)> value_to_bytes,
+      std::function<void(const Key& key)> evict_cb = nullptr)
+      : policy_(ReplacementPolicy::kLRUBytes),
+        capacity_(capacity),
+        value_to_bytes_(std::move(value_to_bytes)),
+        evict_cb_(std::move(evict_cb)) {
+    KATANA_LOG_VASSERT(capacity_ > 0, "cache requires positive capacity");
+    KATANA_LOG_VASSERT(
+        value_to_bytes_ != nullptr,
+        "kLRUBytes policy requires value to bytes function");
+  }
+
+  size_t size() const {
+    if (policy_ == ReplacementPolicy::kLRUSize) {
+      return key_to_value_.size();
+    } else {
+      return total_bytes_;
+    }
+  }
+
+  size_t capacity() const { return capacity_; }
+
+  bool Empty() const { return key_to_value_.empty(); }
+
+  bool Contains(const Key& key) const {
+    return key_to_value_.find(key) != key_to_value_.end();
+  }
+
+  void Insert(const Key& key, const Value& value) {
+    auto mapit = key_to_value_.find(key);
+    if (mapit == key_to_value_.end()) {
+      lru_list_.push_front(key);
+      key_to_value_[key] = {value, lru_list_.begin()};
+      if (value_to_bytes_ != nullptr) {
+        total_bytes_ += value_to_bytes_(value);
+      }
+    } else {
+      mapit->second.value = value;
+      UpdateLRU(mapit);
+    }
+    EvictIfNecessary();
+  }
+
+  std::optional<Value> Get(const Key& key) {
+    // lookup value in the cache
+    std::optional<Value> ret;
+    auto it = key_to_value_.find(key);
+    if (it != key_to_value_.end()) {
+      ret = UpdateLRU(it);
+    }
+    return ret;
+  }
+
+private:
+  Value UpdateLRU(typename MapType::iterator mapit) {
+    auto lru_it = mapit->second.lru_it;
+    auto lru_head = lru_list_.begin();
+    if (lru_it != lru_head) {
+      // move item to the front of the most recently used list
+      lru_list_.splice(lru_head, lru_list_, lru_it);
+      mapit->second.lru_it = lru_head;
+    }
+    return mapit->second.value;
+  }
+
+  void EvictOne() {
+    // evict item from the end of most recently used list
+    auto tail = --lru_list_.end();
+    KATANA_LOG_ASSERT(tail != lru_list_.end());
+    Key evicted_key = std::move(*tail);
+    lru_list_.erase(tail);
+    auto evicted_value = std::move(key_to_value_.at(evicted_key).value);
+    key_to_value_.erase(evicted_key);
+    if (value_to_bytes_ != nullptr) {
+      total_bytes_ -= value_to_bytes_(evicted_value);
+    }
+    if (evict_cb_) {
+      evict_cb_(evicted_key);
+    }
+  }
+
+  void EvictIfNecessary() {
+    switch (policy_) {
+    case ReplacementPolicy::kLRUSize: {
+      while (size() > capacity_) {
+        EvictOne();
+      }
+    } break;
+    case ReplacementPolicy::kLRUBytes: {
+      KATANA_LOG_DEBUG_ASSERT(value_to_bytes_ != nullptr);
+      // Allow a single entry to exceed our byte capacity
+      while (size() > capacity_ && key_to_value_.size() > 1) {
+        EvictOne();
+      }
+    } break;
+    default:
+      KATANA_LOG_FATAL("bad cache replacement policy: {}", policy_);
+    }
+  }
+
+  // Map from key to value
+  MapType key_to_value_;
+  // LRU list
+  ListType lru_list_;
+
+  ReplacementPolicy policy_;
+  // for kLRUSize number of entries kLRUBytes it is byte total
+  size_t capacity_{0};
+  size_t total_bytes_{0};
+
+  std::function<size_t(const Value& value)> value_to_bytes_;
+  std::function<void(const Key& key)> evict_cb_;
+};
+
+}  // namespace tsuba
+
+#endif

349325d91e4a58a50fc50353c802cfc04c76fb1f
Giorgi Lomia <lomiag@bereaalumni.org>
2021-08-25 15:41:12 +0000

    Enforcing the number of entity types as 2

--- libgalois/test/property-file-graph.cpp
+++ libgalois/test/property-file-graph.cpp
@@ -73,9 +73,9 @@ TestTypesFromPropertiesCompareTypesFromStorage() {
   std::string rdg_dir(uri_res.value().path());  // path() because local
 
   KATANA_LOG_VASSERT(
-      g->GetNumNodeEntityTypes() == 1, "found {} entity types.",
+      g->GetNumNodeEntityTypes() == 2, "found {} entity types.",
       g->GetNumNodeEntityTypes());
-  KATANA_LOG_ASSERT(g->GetNumEdgeEntityTypes() == 1);
+  KATANA_LOG_ASSERT(g->GetNumEdgeEntityTypes() == 2);
 
   auto write_result = g->Write(rdg_dir, command_line);
 
@@ -96,9 +96,9 @@ TestTypesFromPropertiesCompareTypesFromStorage() {
   auto g2 = std::move(make_result.value());
 
   KATANA_LOG_VASSERT(
-      g2->GetNumNodeEntityTypes() == 1, "found {} entity types.",
+      g2->GetNumNodeEntityTypes() == 2, "found {} entity types.",
       g2->GetNumNodeEntityTypes());
-  KATANA_LOG_ASSERT(g2->GetNumEdgeEntityTypes() == 1);
+  KATANA_LOG_ASSERT(g2->GetNumEdgeEntityTypes() == 2);
 
   KATANA_LOG_ASSERT((g->node_entity_type_ids_size()) == test_length);
   KATANA_LOG_ASSERT((g->edge_entity_type_ids_size()) == test_length);
@@ -169,9 +169,9 @@ TestCompositeTypesFromPropertiesCompareCompositeTypesFromStorage() {
   std::string rdg_dir(uri_res.value().path());  // path() because local
 
   KATANA_LOG_VASSERT(
-      g->GetNumNodeEntityTypes() != 0, "found {} entity types.",
+      g->GetNumNodeEntityTypes() == 2, "found {} entity types.",
       g->GetNumNodeEntityTypes());
-  KATANA_LOG_ASSERT(g->GetNumEdgeEntityTypes() != 0);
+  KATANA_LOG_ASSERT(g->GetNumEdgeEntityTypes() == 2);
 
   auto write_result = g->Write(rdg_dir, command_line);
 
@@ -192,9 +192,9 @@ TestCompositeTypesFromPropertiesCompareCompositeTypesFromStorage() {
   auto g2 = std::move(make_result.value());
 
   KATANA_LOG_VASSERT(
-      g2->GetNumNodeEntityTypes() != 0, "found {} entity types.",
+      g2->GetNumNodeEntityTypes() == 2, "found {} entity types.",
       g2->GetNumNodeEntityTypes());
-  KATANA_LOG_ASSERT(g2->GetNumEdgeEntityTypes() != 0);
+  KATANA_LOG_ASSERT(g2->GetNumEdgeEntityTypes() == 2);
 
   KATANA_LOG_ASSERT((g->node_entity_type_ids_size()) == test_length);
   KATANA_LOG_ASSERT((g->edge_entity_type_ids_size()) == test_length);

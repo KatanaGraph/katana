5167618326cd19940b4804238293e17c1ed591c3
Evan McGinnis <evan@katanagraph.com>
2021-08-26 13:48:23 -0500

    test load/store new format rdg
    our existing tests only did:
    1) take a graph without EntityTypeID structures
    2) load it
    3) store it in a new location, which stores the new EntityTypeID structures
    4) load from the new location
    
    added a new test that would have caught #488:
    1) take a graph that already has EntityTypeID structures
    2) load it
    3) store it in a new location
    4) load from the new location
    
    Also cleaned up the test structure and added some sanity checks

--- libgalois/test/types-outside-properties.cpp
+++ libgalois/test/types-outside-properties.cpp
@@ -15,51 +15,75 @@ namespace fs = boost::filesystem;
 static cll::opt<std::string> ldbc_003InputFile(
     cll::Positional, cll::desc("<ldbc_003 input file>"), cll::Required);
 
-void
-TestLoadGraphWithoutExternalTypes() {
-  // Load existing "old" graph, which converts all uint8/bool properties into types
-  // store it as a new file
-  // load the new file
-  // ensure the converted old graph, and the loaded new graph match
-  auto g_preconvert_res =
-      katana::PropertyGraph::Make(ldbc_003InputFile, tsuba::RDGLoadOptions());
+katana::PropertyGraph
+LoadGraph(const std::string& rdg_file) {
+  KATANA_LOG_ASSERT(!rdg_file.empty());
+  auto g_res = katana::PropertyGraph::Make(rdg_file, tsuba::RDGLoadOptions());
 
-  if (!g_preconvert_res) {
-    KATANA_LOG_FATAL("making result: {}", g_preconvert_res.error());
+  if (!g_res) {
+    KATANA_LOG_FATAL("making result: {}", g_res.error());
   }
-  std::unique_ptr<katana::PropertyGraph> g_preconvert =
-      std::move(g_preconvert_res.value());
+  katana::PropertyGraph g = std::move(*g_res.value());
+  return g;
+}
 
-  const auto g_node_type_manager = g_preconvert->GetNodeTypeManager();
-
-  const auto g_edge_type_manager = g_preconvert->GetEdgeTypeManager();
-
+std::string
+StoreGraph(katana::PropertyGraph* g){
   auto uri_res = katana::Uri::MakeRand("/tmp/propertyfilegraph");
   KATANA_LOG_ASSERT(uri_res);
   std::string tmp_rdg_dir(uri_res.value().path());  // path() because local
   std::string command_line;
 
-  // Store new converted graph
-  auto write_result = g_preconvert->Write(tmp_rdg_dir, command_line);
-  KATANA_LOG_WARN(
-      "writing converted graph, creating temp file {}", tmp_rdg_dir);
-
+  // Store graph. If there is a new storage format then storing it is enough to bump the version up.
+  KATANA_LOG_WARN("writing graph at temp file {}", tmp_rdg_dir);
+  auto write_result = g->Write(tmp_rdg_dir, command_line);
   if (!write_result) {
     KATANA_LOG_FATAL("writing result failed: {}", write_result.error());
   }
+  return tmp_rdg_dir;
+}
 
-  auto g_postconvert_res =
-      katana::PropertyGraph::Make(tmp_rdg_dir, tsuba::RDGLoadOptions());
-  fs::remove_all(tmp_rdg_dir);
-  if (!g_postconvert_res) {
-    KATANA_LOG_FATAL("making result: {}", g_postconvert_res.error());
-  }
-  std::unique_ptr<katana::PropertyGraph> g_postconvert =
-      std::move(g_postconvert_res.value());
+void
+TestConvertGraphStorageFormat() {
+  // Load existing "old" graph, which converts all uint8/bool properties into types
+  // store it as a new file
+  // load the new file
+  // ensure the converted old graph, and the loaded new graph match
+  katana::PropertyGraph g = LoadGraph(ldbc_003InputFile);
+  std::string g2_rdg_file = StoreGraph(&g);
+  katana::PropertyGraph g2 = LoadGraph(g2_rdg_file);
 
-  KATANA_LOG_WARN("{}", g_preconvert->ReportDiff(g_postconvert.get()));
+  KATANA_LOG_WARN("{}", g.ReportDiff(&g2));
+  KATANA_LOG_ASSERT(g.Equals(&g2));
+}
 
-  KATANA_LOG_ASSERT(g_preconvert->Equals(g_postconvert.get()));
+void
+TestRoundTripNewStorageFormat() {
+  // Test store/load cycle of a graph with the new storage format
+  // To do this, we first must first convert an old graph.
+  // Steps:
+  // Load existing "old" graph, which converts all uint8/bool properties into types
+  // Store it as a new file
+  // Load the new file
+  // Ensure the converted old graph, and the loaded new graph match.
+  // this should be trivially true if TestLoadGraphWithoutExternalTypes() passed
+  // Now store the new graph
+  // Noad the new graph
+
+  // first cycle converts old->new
+  katana::PropertyGraph g = LoadGraph(ldbc_003InputFile);
+  std::string g2_rdg_file = StoreGraph(&g);
+  katana::PropertyGraph g2 = LoadGraph(g2_rdg_file);
+
+  KATANA_LOG_WARN("{}", g.ReportDiff(&g2));
+  KATANA_LOG_ASSERT(g.Equals(&g2));
+
+  // second cycle doesn't do any conversion, but tests storing/loading a "new format" graph
+  std::string g3_rdg_file = StoreGraph(&g2);
+  katana::PropertyGraph g3 = LoadGraph(g3_rdg_file);
+
+  KATANA_LOG_WARN("{}", g2.ReportDiff(&g3));
+  KATANA_LOG_ASSERT(g2.Equals(&g3));
 }
 
 int
@@ -67,7 +91,8 @@ main(int argc, char** argv) {
   katana::SharedMemSys sys;
   cll::ParseCommandLineOptions(argc, argv);
 
-  TestLoadGraphWithoutExternalTypes();
+  TestConvertGraphStorageFormat();
+  TestRoundTripNewStorageFormat();
 
   return 0;
 }

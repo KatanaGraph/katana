16c4f3dad7281ac4acc12ae7ca362c92738a5b4d
simon <simon@katanagraph.com>
2021-09-28 21:43:29 +0000

    RDGSlice: offer a richer interface
    Take advantage of newly enriched RDGCore interface to provde a richer
    interface in RDGSlice. This allows RDGSlice to load slices of RDGs that
    are a single partition in a partitioned graph.

--- libtsuba/include/tsuba/RDGSlice.h
+++ libtsuba/include/tsuba/RDGSlice.h
@@ -10,6 +10,7 @@
 #include "katana/URI.h"
 #include "katana/config.h"
 #include "tsuba/FileView.h"
+#include "tsuba/RDGLineage.h"
 #include "tsuba/tsuba.h"
 
 namespace tsuba {
@@ -17,7 +18,15 @@ namespace tsuba {
 class RDGManifest;
 class RDGCore;
 
-/// A contiguous piece of an RDG
+/// A read-only contiguous piece of a partition.
+///
+/// This class owns a contiguous slice of nodes from the associated default CSR
+/// topology. It also owns the outgoing edges associated with those nodes.
+/// Because of the structure of CSR, those edges form a contiguous slice.
+/// As a result, it also owns a contiguous slice from each of
+/// the node and edge property arrays and the node and edge type id arrays.
+///
+/// A typical use would be loading an unpartitioned graph on multiple hosts.
 class KATANA_EXPORT RDGSlice {
 public:
   RDGSlice(const RDGSlice& no_copy) = delete;
@@ -44,9 +53,47 @@ public:
       const std::optional<std::vector<std::string>>& node_props = std::nullopt,
       const std::optional<std::vector<std::string>>& edge_props = std::nullopt);
 
+  // metadata sorts of things
+  const katana::Uri& rdg_dir() const;
+  uint32_t partition_id() const;
+
+  // properties and friends
+  std::shared_ptr<arrow::Schema> full_node_schema() const;
+  std::shared_ptr<arrow::Schema> full_edge_schema() const;
   const std::shared_ptr<arrow::Table>& node_properties() const;
   const std::shared_ptr<arrow::Table>& edge_properties() const;
+
+  // topology and friends
   const FileView& topology_file_storage() const;
+
+  // optional partition metadata
+  const std::vector<std::shared_ptr<arrow::ChunkedArray>>& master_nodes()
+      const {
+    return master_nodes_;
+  }
+  const std::vector<std::shared_ptr<arrow::ChunkedArray>>& mirror_nodes()
+      const {
+    return mirror_nodes_;
+  }
+  const std::shared_ptr<arrow::ChunkedArray>& host_to_owned_global_node_ids()
+      const {
+    return host_to_owned_global_node_ids_;
+  }
+  const std::shared_ptr<arrow::ChunkedArray>& host_to_owned_global_edge_ids()
+      const {
+    return host_to_owned_global_edge_ids_;
+  }
+  const std::shared_ptr<arrow::ChunkedArray>& local_to_user_id() const {
+    return local_to_user_id_;
+  }
+  const std::shared_ptr<arrow::ChunkedArray>& local_to_global_id() const {
+    return local_to_global_id_;
+  }
+
+  // type info
+  /// Determine if the EntityTypeIDs are stored in properties, or outside
+  /// in their own dedicated structures
+  bool IsEntityTypeIDsOutsideProperties() const;
   const FileView& node_entity_type_id_array_file_storage() const;
   const FileView& edge_entity_type_id_array_file_storage() const;
   katana::Result<katana::EntityTypeManager> node_entity_type_manager() const;
@@ -69,6 +116,20 @@ private:
   //
 
   std::unique_ptr<RDGCore> core_;
+  // NB: we intentionally do not include a property cache here because that will
+  // complicate the property cache logic; one could be added in the future if it
+  // seems necessary
+
+  void InitArrowVectors();
+  std::vector<std::shared_ptr<arrow::ChunkedArray>> mirror_nodes_;
+  std::vector<std::shared_ptr<arrow::ChunkedArray>> master_nodes_;
+  std::shared_ptr<arrow::ChunkedArray> host_to_owned_global_node_ids_;
+  std::shared_ptr<arrow::ChunkedArray> host_to_owned_global_edge_ids_;
+  std::shared_ptr<arrow::ChunkedArray> local_to_user_id_;
+  std::shared_ptr<arrow::ChunkedArray> local_to_global_id_;
+
+  // How this graph was derived from the previous version
+  RDGLineage lineage_;
 };
 
 }  // namespace tsuba

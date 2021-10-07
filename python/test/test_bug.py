741d676fb86707eae4b8dcd63a4e7fcf938cd659
Denish Patel <86317972+denish44@users.noreply.github.com>
2021-10-07 12:07:58 -0500

    Missing command is not an error for capture_command  (#600)
    Missing command is not an error for capture_command
    
    capture_command is to aid debugging possibly broken build environments so it should complete executing even if the build environment is missing expected programs.

--- python/test/test_bug.py
+++ python/test/test_bug.py
@@ -13,3 +13,11 @@ def test_bug_capture_environment():
             files = {f.filename for f in zipin.filelist}
             for fn in ["info/os.txt", "info/python.txt", "info/conda.txt", "info/cmake.txt", "etc/ld.so.conf"]:
                 assert fn in files
+
+
+def test_bug_capture_command_pass_invalid_command():
+    from katana.bug.environment import capture_command
+
+    # Make sure it doesn't fail for non existent command
+    result = capture_command("non-existent-executable", "xyz")
+    assert isinstance(result, str)

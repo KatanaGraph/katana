9fa7341382d0ca8b37483421cea8f4a3a2d78c45
aman <aman@odachi.katanagraph.com>
2021-10-18 19:26:46 +0000

    temporal types should match

--- libsupport/src/ArrowVisitor.cpp
+++ libsupport/src/ArrowVisitor.cpp
@@ -24,7 +24,7 @@ public:
       (arrow::is_boolean_type<ScalarArrowType>::value &&
        arrow::is_boolean_type<BuilderArrowType>::value) ||
       (arrow::is_temporal_type<ScalarArrowType>::value &&
-       arrow::is_temporal_type<BuilderArrowType>::value);
+       std::is_same<ScalarArrowType, BuilderArrowType>::value);
 
   template <typename ScalarArrowType, typename BuilderArrowType>
   static constexpr bool MatchString =

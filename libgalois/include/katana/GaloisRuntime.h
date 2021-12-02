dbf60d4aa40bace5682669d4fab075c7cdbca635
M. Amber Hassaan <amber_474@yahoo.com>
2021-12-02 09:47:45 +0000

    Lint fixes

--- libgalois/include/katana/GaloisRuntime.h
+++ libgalois/include/katana/GaloisRuntime.h
@@ -17,8 +17,8 @@
  * Documentation, or loss or inaccuracy of data of any kind.
  */
 
-#ifndef KATANA_LIBGALOIS_KATANA_GALOISRT_H_
-#define KATANA_LIBGALOIS_KATANA_GALOISRT_H_
+#ifndef KATANA_LIBGALOIS_KATANA_GALOISRUNTIME_H_
+#define KATANA_LIBGALOIS_KATANA_GALOISRUNTIME_H_
 
 #include <memory>
 

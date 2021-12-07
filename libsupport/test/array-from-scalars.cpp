a7aae7932c1ab87c512e2eda19fd1d5befd08d95
danielmawhirter <daniel@mawhirter.com>
2021-12-07 22:03:34 +0000

    use arrow 6.0 capabilities to generalize ArrayFromScalars

--- libsupport/test/array-from-scalars.cpp
+++ libsupport/test/array-from-scalars.cpp
@@ -23,8 +23,27 @@ TestNulls() {
 }
 
 katana::Result<void>
+TestMixed() {
+  std::vector<std::shared_ptr<arrow::Scalar>> scalars(NUM_ENTRIES);
+  scalars[3] = std::make_shared<arrow::LargeStringScalar>("three");
+  scalars[4] = std::make_shared<arrow::LargeStringScalar>("four");
+  scalars[9] = std::make_shared<arrow::LargeStringScalar>("nine");
+  std::shared_ptr<arrow::Array> array =
+      KATANA_CHECKED(katana::ArrayFromScalars(scalars, arrow::large_utf8()));
+  // NB: length() is signed, size() is not - but they should both have small
+  // enough values that it doesn't matter
+  KATANA_LOG_VASSERT(
+      static_cast<size_t>(array->length()) == scalars.size(),
+      "array length: {}, vector size: {}", array->length(), scalars.size());
+  KATANA_LOG_ASSERT(array->length() == NUM_ENTRIES);
+  KATANA_LOG_ASSERT(array->null_count() == NUM_ENTRIES - 3);
+  return katana::ResultSuccess();
+}
+
+katana::Result<void>
 TestAll() {
   KATANA_CHECKED(TestNulls());
+  KATANA_CHECKED(TestMixed());
   return katana::ResultSuccess();
 }
 

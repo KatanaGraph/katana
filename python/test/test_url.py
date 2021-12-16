148515b076e99af6525bb3a6a9a31c833d33f818
Karthik Velayutham <vkarthik@katanagraph.com>
2021-12-16 23:42:12 +0000

    Added test

---
+++ python/test/test_url.py
@@ -0,0 +1,22 @@
+from katana import url
+
+
+def test_url_join_path():
+    assert url.URL._join_path("file:///home/", "a", "b") == "file:///home/a/b"
+    assert url.URL._join_path("file:///home?query=string", "a") == "file:///home/a?query=string"
+    assert url.URL._join_path("file:///home", "a") == "file:///home/a"
+    assert url.URL._join_path("file:///home/", "a") == "file:///home/a"
+    assert url.URL._join_path("file:///home/", "a/") == "file:///home/a/"
+    assert url.URL._join_path("file:///home/") == "file:///home/"
+
+
+def test_url_div_op():
+    assert str(url.URL("file:///home/") / "a" / "b") == "file:///home/a/b"
+    assert str(url.URL("file:///home/") / "a/b") == "file:///home/a/b"
+    assert str(url.URL("file:///home/", "a", "b")) == "file:///home/a/b"
+    assert str(url.URL("file:///home?query=string") / "a") == "file:///home/a?query=string"
+    assert url.URL("file:///home") / "a" == url.URL("file:///home/a")
+    assert url.URL("file:///home/") / "a" == url.URL("file:///home/a")
+    assert str("file:///home/" / url.URL("a/")) == "file:///home/a/"
+    assert str(url.URL("file:///home/")) == "file:///home/"
+    assert str(url.URL("a") / "b.txt") == "a/b.txt"

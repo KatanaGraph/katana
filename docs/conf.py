f378dbfc739cd62b2da66c6fe145088f0c41dd25
Simon Bertron <32773665+scober@users.noreply.github.com>
2022-01-05 14:07:19 -0700

    Revert "Docs: allow some broken links in open documentation"
    This reverts commit fd6d1ac32ae0c03249d4756b1a36799ab146502b.

--- docs/conf.py
+++ docs/conf.py
@@ -82,11 +82,8 @@ html_css_files = ["style.css"]
 autodoc_preserve_defaults = True
 autodoc_member_order = "groupwise"
 
-# the open documentation links to the enterprise documentation in some places
-# let those links be broken in the open-only documentation and don't complain
-suppress_warnings = ["ref.ref"]
+suppress_warning = []
 nitpick_ignore = []
-
 
 # Exclude drafts from external documentation
 if tags.has("external"):

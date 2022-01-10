070f1b925862518f32d39e47afe7507b66e92048
Arthur Peters <amp@katanagraph.com>
2022-01-10 13:42:58 -0600

    Ooops. Misused the old style constructor.

--- libkatana_python_native/src/Reductions.cpp
+++ libkatana_python_native/src/Reductions.cpp
@@ -53,9 +53,13 @@ struct ReducibleFunctor {
         "This class can be passed into numba compiled code and it's methods "
         "can be used from there.\n");
     cls.def(py::init<>());
+    // TODO(amp): Remove once we drop support for older pybind11 on ubuntu 18.04
     cls.def(
         "__init__",
-        [](typename For::template type<T>& self, T v) { self.update(v); },
+        [](typename For::template type<T>& self, T v) {
+          new (&self) typename For::template type<T>();
+          self.update(v);
+        },
         "Create a new instance with an initial value.");
     katana::RegisterNumbaClass(cls);
     katana::DefWithNumba<py::overload_cast<const T&>(

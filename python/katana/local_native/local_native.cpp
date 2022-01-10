e577bc61c25f295c714000575b6945d390a08efc
Arthur Peters <amp@katanagraph.com>
2022-01-10 12:35:09 -0600

    More pybind11 2.0 support

--- python/katana/local_native/local_native.cpp
+++ python/katana/local_native/local_native.cpp
@@ -1,4 +1,14 @@
 #include <katana/python/PythonModuleInitializers.h>
 #include <pybind11/pybind11.h>
 
+#ifdef PYBIND11_MODULE
+// pybind11 >= 2.2
 PYBIND11_MODULE(local_native, m) { katana::python::InitReductions(m); }
+#else
+// pybind11 < 2.2
+PYBIND11_PLUGIN(local_native) {
+  pybind11::module m("local_native");
+  katana::python::InitReductions(m);
+  return m.ptr();
+}
+#endif

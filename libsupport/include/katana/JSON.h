f02437d190013402663856c44a3c110e6fadd70f
Daniel Mawhirter <danielmawhirter@users.noreply.github.com>
2022-01-24 17:58:35 -0600

    allow nlohmann::ordered_json in JsonDump KAT-1157 (#799) (#830)
    allow nlohmann::ordered_json to use the same DumpJSON interface as nlohmann::json
    required for the (nonstandard json) ordering of keys in query result according to return order
    
    KAT-1157

--- libsupport/include/katana/JSON.h
+++ libsupport/include/katana/JSON.h
@@ -37,6 +37,8 @@ JsonParse(U& obj, T* val) {
 
 /// Dump to string, but catch errors
 KATANA_EXPORT katana::Result<std::string> JsonDump(const nlohmann::json& obj);
+KATANA_EXPORT katana::Result<std::string> JsonDump(
+    const nlohmann::ordered_json& obj);
 
 template <typename T>
 katana::Result<std::string>

213001d52cbec9036dda8fee53f8bc71d76676dd
pyrito <pyrito@users.noreply.github.com>
2022-01-24 19:22:56 +0000

    deploy: 1275eb20661b9af86320c9cb3abbcc56bf2ba237

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.102.dev+122.0.ddf1de',
+    VERSION: '0.1.102.dev+123.0.1275eb',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

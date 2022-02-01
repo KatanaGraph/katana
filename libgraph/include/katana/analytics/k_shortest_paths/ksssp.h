3cb15364f0200d294c3a9967c529eb9da6090a40
Daryan Qawami <daryan.qawami@katanagraph.com>
2022-01-31 20:18:31 -0800

    Enable Views for K-Shortest-Paths (#821)
    Changed k-shortest-paths to enable views for inputted graphs
    
    Co-authored-by: daryan.qawami <daryan.qawami@tachi.katanagraph.com>

---
+++ libgraph/include/katana/analytics/k_shortest_paths/ksssp.h
@@ -0,0 +1,21 @@
+#ifndef KATANA_LIBGRAPH_KATANA_ANALYTICS_KSHORTESTPATHS_KSSSP_H_
+#define KATANA_LIBGRAPH_KATANA_ANALYTICS_KSHORTESTPATHS_KSSSP_H_
+
+#include "katana/analytics/sssp/sssp.h"
+
+namespace katana::analytics {
+using kSsspPlan = SsspPlan;
+
+enum AlgoReachability { async = 0, syncLevel };
+
+/// Compute the K Shortest Path for pg starting from start_node.
+/// The algorithm and delta stepping
+/// parameter can be specified, but have reasonable defaults.
+KATANA_EXPORT Result<void> Ksssp(
+    katana::PropertyGraph* pg, const std::string& edge_weight_property_name,
+    uint32_t start_node, uint32_t report_node, katana::TxnContext* txn_ctx,
+    AlgoReachability algo_reachability, uint32_t num_paths, uint32_t step_shift,
+    const bool& is_symmetric, kSsspPlan plan = {});
+}  // namespace katana::analytics
+
+#endif

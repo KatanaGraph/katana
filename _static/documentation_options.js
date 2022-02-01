048fa0a9f04569a94d416322484f996d294deae7
denish44 <denish44@users.noreply.github.com>
2022-02-01 21:58:11 +0000

    deploy: 6d1efd12eda0fc6a0365f3618002ccaa97bcbad3

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.102.dev+139.0.1a6d20',
+    VERSION: '0.1.102.dev+140.0.6d1efd',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

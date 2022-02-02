2ab728de671317bc59c323098d3b1d6089808f02
Arthur Peters <amp@katanagraph.com>
2022-02-02 16:05:24 -0600

    Make instantiation selection totally configurable in TemplateSupport.h

--- libkatana_python_native/include/katana/python/TemplateSupport.h
+++ libkatana_python_native/include/katana/python/TemplateSupport.h
@@ -8,6 +8,12 @@ namespace katana {
 
 // TODO(amp): Use template template parameters once we can rely on having them.
 
+template <typename T>
+std::string
+GetInstantiationName(const std::string& basename) {
+  return basename + "[" + PythonTypeTraits<T>::name + "]";
+}
+
 /// Invoke @c f&lt;T&gt;(m, name) and assign the resulting class into
 /// @c types[T].
 ///
@@ -22,11 +28,39 @@ pybind11::object
 InstantiateForType(
     pybind11::module m, const std::string& basename, F f,
     pybind11::dict types) {
-  pybind11::object dtype = katana::PythonTypeTraits<T>::default_dtype();
-  pybind11::object cls = f.template instantiate<T>(
-      m, (basename + "[" + katana::PythonTypeTraits<T>::name + "]").c_str());
-  types[dtype] = cls;
+  pybind11::object representation =
+      katana::PythonTypeTraits<T>::representation();
+  pybind11::object cls =
+      f.template instantiate<T>(m, GetInstantiationName<T>(basename).c_str());
+  types[representation] = cls;
   return cls;
+}
+
+template <typename... Types, typename F>
+void
+InstantiateForTypes(pybind11::module m, const std::string& basename, F f) {
+  pybind11::module builtins = pybind11::module::import("builtins");
+  auto make_template_type1 = pybind11::cast<pybind11::function>(
+      pybind11::module::import("katana.native_interfacing.template_type")
+          .attr("make_template_type1"));
+  pybind11::dict types;
+
+  auto t = std::make_tuple(InstantiateForType<Types>(m, basename, f, types)...);
+
+  // Set the builtin type int as an alias for int64 if int64 exists.
+  if (types.template contains(
+          katana::PythonTypeTraits<int64_t>::representation())) {
+    types[builtins.attr("int")] =
+        types[katana::PythonTypeTraits<int64_t>::representation()];
+  }
+  // Set the builtin type float as an alias for float64/double if double exists.
+  if (types.template contains(
+          katana::PythonTypeTraits<double>::representation())) {
+    types[builtins.attr("float")] =
+        types[katana::PythonTypeTraits<double>::representation()];
+  }
+
+  m.attr(basename.c_str()) = make_template_type1(basename, types);
 }
 
 /// Invoke `f<T>(m, fullname)` for a set of standard types and store the
@@ -37,30 +71,13 @@ InstantiateForType(
 ///     type name.
 /// \param f The functor which instantiates the class. It must provide a method
 ///     @c instantiate&lt;T&gt; which instantiates and wraps the type at @c T.
-template <typename F>
+template <typename... Types, typename F>
 void
 InstantiateForStandardTypes(
     pybind11::module m, const std::string& basename, F f) {
-  pybind11::module builtins = pybind11::module::import("builtins");
-  auto make_template_type1 = pybind11::cast<pybind11::function>(
-      pybind11::module::import("katana.native_interfacing.template_type")
-          .attr("make_template_type1"));
-  pybind11::dict types;
-  InstantiateForType<uint8_t>(m, basename, f, types);
-  InstantiateForType<uint16_t>(m, basename, f, types);
-  InstantiateForType<uint32_t>(m, basename, f, types);
-  InstantiateForType<uint64_t>(m, basename, f, types);
-  InstantiateForType<int8_t>(m, basename, f, types);
-  InstantiateForType<int16_t>(m, basename, f, types);
-  InstantiateForType<int32_t>(m, basename, f, types);
-  // Set the builtin type int as an alias for int64
-  types[builtins.attr("int")] =
-      InstantiateForType<int64_t>(m, basename, f, types);
-  InstantiateForType<float>(m, basename, f, types);
-  // Set the builtin type float as an alias for float64/double
-  types[builtins.attr("float")] =
-      InstantiateForType<double>(m, basename, f, types);
-  m.attr(basename.c_str()) = make_template_type1(basename, types);
+  InstantiateForTypes<
+      uint8_t, uint16_t, uint32_t, uint64_t, int8_t, int16_t, int32_t, int64_t,
+      float, double>(m, basename, f);
 }
 
 }  // namespace katana

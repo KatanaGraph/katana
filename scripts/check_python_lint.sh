ac9b36624435f2e34cc4466f0655db44fcd15d57
Arthur Peters <amp@katanagraph.com>
2022-02-03 22:29:37 -0600

    Replace python checkers tool with a parallel python implemention (#864)
    * Print tool versions by default.

--- scripts/check_python_lint.sh
+++ scripts/check_python_lint.sh
@@ -1,31 +1,6 @@
 #!/bin/bash
 
-set -eu
+export GIT_ROOT=$(readlink -f $(dirname $0)/..)
 
-GIT_ROOT=$(readlink -f $(dirname $0)/..)
-
-if [ $# -eq 0 ]; then
-  echo "$(basename $0) <paths>" >&2
-  exit 1
-fi
-
-LINT=${PYLINT:-pylint}
-ARGS="-j 0 --rcfile=${GIT_ROOT}/pyproject.toml"
-ROOTS="$@"
-FAILED=
-PRUNE_LIST="build .git"
-
-emit_prunes() {
-  for p in ${PRUNE_LIST}; do echo "-name ${p} -prune -o"; done | xargs
-}
-
-while read -d '' filename; do
-  if ! ${LINT} ${ARGS} "${filename}"; then
-    echo "${filename} NOT OK"
-    FAILED=1
-  fi
-done < <(find ${ROOTS} $(emit_prunes) -name '*.py' -print0)
-
-if [ -n "${FAILED}" ]; then
-  exit 1
-fi
+BASE="$(cd "$(dirname "$(readlink -f "$0")")"; pwd)"
+"$BASE"/check_python.py --pylint "$@"

6817344ebdcff9c90bc4a4ba34425974b89dc1df
Daryan Qawami <daryan.qawami@katanagraph.com>
2022-02-10 12:22:31 -0800

    Changed default plan

--- python/katana/local/analytics/_ksssp.pyx
+++ python/katana/local/analytics/_ksssp.pyx
@@ -229,7 +229,7 @@ cdef class AlgoReachability:
 def ksssp(pg, str edge_weight_property_name, size_t start_node, 
           size_t report_node, size_t num_paths, bool is_symmetric=False, 
           AlgoReachability algo_reachability = AlgoReachability(), 
-          KssspPlan plan = KssspPlan(), *, txn_ctx = None):
+          KssspPlan plan = KssspPlan().delta_tile(), *, txn_ctx = None):
     """
     Compute the K-Shortest Path on `pg` using `start_node` as source.
 

67b08aa9990842959e3ef69377d3faeb47fbe02d
Daryan Qawami <daryan.qawami@katanagraph.com>
2022-02-10 20:27:04 -0800

    Added const

--- libgraph/src/analytics/k_shortest_paths/ksssp.cpp
+++ libgraph/src/analytics/k_shortest_paths/ksssp.cpp
@@ -476,7 +476,7 @@ template <typename Weight>
 katana::Result<void>
 kSSSPWithWrap(
     katana::PropertyGraph* pg, const std::string& edge_weight_property_name,
-    std::string& output_property_name, size_t start_node, size_t report_node, 
+    const std::string& output_property_name, size_t start_node, size_t report_node, 
     size_t num_paths, const bool& is_symmetric, katana::TxnContext* txn_ctx,
     AlgoReachability algo_reachability, kSsspPlan plan) {
   if (!edge_weight_property_name.empty() &&

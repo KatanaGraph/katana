e873d0f6379001b68cabc79dee28ff9f32bb1086
Daryan Qawami <daryan.qawami@katanagraph.com>
2022-02-11 01:07:57 -0800

    Simplified symmetric check

--- libgraph/src/analytics/k_shortest_paths/ksssp.cpp
+++ libgraph/src/analytics/k_shortest_paths/ksssp.cpp
@@ -520,7 +520,7 @@ kSSSPWithWrap(
   KATANA_CHECKED(pg->ConstructNodeProperties<NodeData<Weight>>(
       txn_ctx, {temp_node_property_names}));
 
-  using GraphType = (is_symmetric) ? katana::PropertyGraphViews::Default : katana::PropertyGraphViews::Undirected;
+  typename GraphType = (is_symmetric) ? katana::PropertyGraphViews::Default : katana::PropertyGraphViews::Undirected;
 
   using Graph = katana::TypedPropertyGraphView<
       GraphType, NodeData<Weight>, EdgeData<Weight>>;

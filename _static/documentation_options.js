91468805e99299f14d539d86171ced440f99d474
witchel <witchel@users.noreply.github.com>
2022-02-14 18:36:32 +0000

    deploy: f94fb7e4c30cb00620a7f8a8e8b2c21904a20154

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.102.dev+194.0.c0267a',
+    VERSION: '0.1.102.dev+195.0.f94fb7',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

e792950d3f480f26c92c77931e094fdb9e3d3649
mindv0rtex <mindv0rtex@users.noreply.github.com>
2022-02-22 22:31:37 +0000

    deploy: 726b8b92a6dd25ce9733723c86ddbc6b8aad4e5d

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.102.dev+223.0.95ad4c',
+    VERSION: '0.1.102.dev+224.0.726b8b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

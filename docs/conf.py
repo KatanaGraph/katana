9d2d4b73988746da14d3e627fbe26983352387b6
Nick consta <nick.consta@oneteam.us>
2022-02-22 17:12:51 -0500

    Merging master into High Seas fork

--- docs/conf.py
+++ docs/conf.py
@@ -86,7 +86,7 @@ html_theme_options = {
 }
 # html_theme_path = []
 
-# Add any paths that contain custom static files (such as style sheets) here,
+# Add any paths that contain custom static files (such as style sheets) here,gi
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]

fccdae122fe113d2633ad0f1c7906a6dd0cf9bd7
arthurp <arthurp@users.noreply.github.com>
2022-02-24 19:50:35 +0000

    deploy: 862ef59affe9d2017df14d06701fd1032511ba1a

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.102.dev+228.0.d26964',
+    VERSION: '0.1.102.dev+229.0.862ef5',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

484d4366733a20b32d9ee6eb493b1fa19169afd6
Daryan Qawami <daryan.qawami@katanagraph.com>
2022-03-05 14:25:39 -0800

    Making Print return katana::Result<void> to get errors

--- libgraph/src/analytics/k_shortest_paths/ksssp.cpp
+++ libgraph/src/analytics/k_shortest_paths/ksssp.cpp
@@ -580,9 +580,9 @@ katana::analytics::Ksssp(
   }
 }
 
-void
+katana::Result<void>
 katana::analytics::KssspStatistics::Print() const {
-  katana::URI uri = KATANA_CHECKED(URI::Make("./output"));
+  katana::URI uri = KATANA_CHECKED(URI::Make("./output.txt"));
   auto opts = katana::ParquetWriter::WriteOpts::Defaults();
   opts.parquet_version = parquet::ParquetVersion::PARQUET_1_0;
   opts.data_page_version = parquet::ParquetDataPageVersion::V1;

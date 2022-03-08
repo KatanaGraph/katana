ffc75ddea41f4f65fdbdea0618a0a4050748860d
JamesFCoffey <JamesFCoffey@users.noreply.github.com>
2022-03-08 21:59:44 +0000

    deploy: cf57180ed4e8f0d04046cdd3a55f61c4b9107c56

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.0.dev+2.0.70337b',
+    VERSION: '0.3.0.dev+3.0.cf5718',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

fb6a2f6878e2237df54fb0651cf8f59477f78ca8
tylershunt <tylershunt@users.noreply.github.com>
2022-03-08 20:46:56 +0000

    deploy: 70337b2451c973ca1d856381705b10b2f0038730

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.0.dev+1.0.17a12f',
+    VERSION: '0.3.0.dev+2.0.70337b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

ba6ee9f014302a8720e8114df921ef37eb1b67b4
micah-white <micah-white@users.noreply.github.com>
2022-03-10 23:33:15 +0000

    deploy: 1ffc8505e0f81dda7618a8881b3459bbc4d43bb2

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.0.dev+5.0.76bfe7',
+    VERSION: '0.3.0.dev+6.0.1ffc85',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

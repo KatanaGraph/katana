399f13c2b24eeb90923dec14bfece9e9c69c1258
danielmawhirter <danielmawhirter@users.noreply.github.com>
2022-03-12 21:35:14 +0000

    check-in

--- libgraph/include/katana/HashIndexedProperty.h
+++ libgraph/include/katana/HashIndexedProperty.h
@@ -1,6 +1,10 @@
 #ifndef KATANA_LIBGRAPH_KATANA_HASHINDEXEDPROPERTY_H_
 #define KATANA_LIBGRAPH_KATANA_HASHINDEXEDPROPERTY_H_
 
+#include <unordered_map>
+
+#include "katana/Result.h"
+
 namespace katana {
 
 class KATANA_EXPORT HashIndexedProperty {
@@ -12,10 +16,15 @@ public:
   Result<std::shared_ptr<arrow::Array>> Inflate() const;
 
 private:
-  HashIndexedProperty(HashMap index, std::shared_ptr<arrow::Array> data)
-      : index_(std::move(index)), data_(std::move(data)) {}
+  HashIndexedProperty(
+      HashMap index, std::shared_ptr<arrow::Array> data, int64_t length)
+      : index_(std::move(index)), data_(std::move(data)), length_(length) {}
+
+  Result<std::shared_ptr<arrow::Array>> MakeDenseMap() const;
+
   HashMap index_;
   std::shared_ptr<arrow::Array> data_;
+  int64_t length_;
 };
 
 }  // namespace katana
--- libgraph/src/HashIndexedProperty.cpp
+++ libgraph/src/HashIndexedProperty.cpp
@@ -1,5 +1,8 @@
 #include "katana/HashIndexedProperty.h"
+
 #include <arrow/compute/api.h>
+
+#include "katana/ArrowRandomAccessBuilder.h"
 
 katana::Result<katana::HashIndexedProperty>
 katana::HashIndexedProperty::Deflate(const arrow::Array& array) {
@@ -12,12 +15,22 @@ katana::HashIndexedProperty::Deflate(const arrow::Array& array) {
   }
 
   std::shared_ptr<arrow::Array> data =
-      KATANA_CHECKED(arrow::compute::DropNulls(array));
+      KATANA_CHECKED(arrow::compute::DropNull(array));
 
-  return HashIndexedProperty(std::move(index), std::move(data));
+  return HashIndexedProperty(std::move(index), std::move(data), array.length());
 }
 
 katana::Result<std::shared_ptr<arrow::Array>>
 katana::HashIndexedProperty::Inflate() const {
+  std::shared_ptr<arrow::Array> dense_map = KATANA_CHECKED(MakeDenseMap());
+  return KATANA_CHECKED(arrow::compute::Take(*dense_map, *data_));
+}
 
+katana::Result<std::shared_ptr<arrow::Array>>
+katana::HashIndexedProperty::MakeDenseMap() const {
+  katana::ArrowRandomAccessBuilder<arrow::Int64Type> builder(length_);
+  for (auto [k, v] : index_) {
+    builder[k] = v;
+  }
+  return builder.Finalize();
 }

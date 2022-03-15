62daf462de0d90b34d99c5afb429374788423b2f
yshalabi <yshalabi@users.noreply.github.com>
2022-03-15 14:38:09 +0000

    deploy: bb2ade2c9a91cbce9379618fc65ca56f8dd8c0f1

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.0.dev+8.0.c0838f',
+    VERSION: '0.3.0.dev+9.0.bb2ade',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

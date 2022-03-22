063c6685ba9cb729a5b7b6c25c096dc2b5bc0c36
Daryan Qawami <daryan.qawami@katanagraph.com>
2022-03-22 12:11:32 -0700

    Added check back in for res

--- python/katana/local/analytics/_ksssp.pyx
+++ python/katana/local/analytics/_ksssp.pyx
@@ -224,8 +224,8 @@ def ksssp(pg, str edge_weight_property_name, size_t start_node,
         res = Ksssp(underlying_property_graph(pg), edge_weight_property_name_str,
                     start_node, report_node, num_paths, is_symmetric,
                     underlying_txn_context(txn_ctx), plan.underlying_)
-    """if not res.has_value():
-        raise_error_code(res.error())"""
+    if not res.has_value():
+        raise_error_code(res.error())
     return pyarrow_wrap_table(res.value())
 
 

8a4988fc8d4c30a59f9132fc879e6099e23f7e2e
ddn0 <ddn0@users.noreply.github.com>
2022-03-22 19:14:50 +0000

    deploy: 248b57840e0cbf949f683fb59fb180dbb2b771b4

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.0.dev+17.0.ba6305',
+    VERSION: '0.3.0.dev+18.0.248b57',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

ad002c65122bda934c2e42062a241b46052e0741
dqawami <dqawami@users.noreply.github.com>
2022-03-26 00:43:29 +0000

    deploy: f2ac5d75dc56f803166d381a79d23c7a357281db

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.0.dev+26.0.1f484e',
+    VERSION: '0.3.0.dev+27.0.f2ac5d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

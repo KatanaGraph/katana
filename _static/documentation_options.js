6a8830257eee9b3e6b5217024a02f400e1ffed54
arekay <arekay@users.noreply.github.com>
2022-03-29 01:41:04 +0000

    deploy: fc9f862394a853b5b243b05a0982ef610991706c

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.0.dev+29.0.8531be',
+    VERSION: '0.3.0.dev+30.0.fc9f86',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
